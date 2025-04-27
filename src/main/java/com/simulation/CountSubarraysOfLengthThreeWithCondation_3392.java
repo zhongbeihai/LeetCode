518a5c537436659f483ca39a3dc50320d8c8c721
logan <huang1002788835@outlook.com>
2025-04-26 22:48:09 -0700

    3392, 2444

---
+++ src/main/java/com/simulation/CountSubarraysOfLengthThreeWithCondation_3392.java
@@ -0,0 +1,15 @@
+package com.simulation;
+
+public class CountSubarraysOfLengthThreeWithCondation_3392 {
+    public int countSubarrays(int[] nums) {
+        int res = 0;
+        if (nums.length < 3) return 0;
+        for (int i = 2; i < nums.length; i++){
+            int mid = nums[i -1];
+            int lr = nums[i - 2] + nums[i];
+            if (mid == lr * 2) res++;
+        }
+
+        return res;
+    }
+}
---
+++ src/main/java/com/sliding_windows/CountSubarrayWithFixedBounds_2444.java
@@ -0,0 +1,42 @@
+package com.sliding_windows;
+
+import javafx.util.BuilderFactory;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class CountSubarrayWithFixedBounds_2444 {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int res = 0, n = nums.length;
+        int left = 0;
+        Deque<Integer> minQue = new ArrayDeque<>();
+        Deque<Integer> maxQue = new ArrayDeque<>();
+
+        for(int right = 0; right < n; right++){
+            if (nums[right] < minK || nums[right] > maxK){
+                minQue.clear();
+                maxQue.clear();
+                left = right + 1;
+                continue;
+            }
+
+            while (!minQue.isEmpty() && nums[minQue.peekLast()] >= nums[right]) minQue.pollLast();
+            minQue.addLast(right);
+            while (!maxQue.isEmpty() && nums[maxQue.peekLast()] <= nums[right] ) maxQue.pollLast();
+            maxQue.addLast(right);
+
+            if (nums[minQue.peekFirst()] == minK && nums[maxQue.peekFirst()] == maxK) {
+                int start = Math.min(minQue.peekFirst(), maxQue.peekFirst());
+                res += (start - left + 1);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        CountSubarrayWithFixedBounds_2444 c = new CountSubarrayWithFixedBounds_2444();
+        c.countSubarrays(new int[]{3,1,5,2,7,5}, 1, 5);
+    }
+}
