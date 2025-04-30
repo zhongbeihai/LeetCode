ea693f804b6dd11464f6e8eb3aa8d37a3045c8f9
logan <huang1002788835@outlook.com>
2025-04-29 21:04:30 -0700

    1295

---
+++ src/main/java/com/sliding_windows/FindNumbersWithEvenNumberOfDigits_1295.java
@@ -0,0 +1,14 @@
+package com.sliding_windows;
+
+public class FindNumbersWithEvenNumberOfDigits_1295 {
+    public int findNumbers(int[] nums) {
+        int res = 0;
+        for (int num: nums){
+            int digits = String.valueOf(num).length();
+            if (digits % 2 == 0) res++;
+        }
+
+        return res;
+    }
+
+}
