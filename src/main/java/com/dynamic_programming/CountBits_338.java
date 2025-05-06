93632da0223eabdc53a851b5870b5063259ea680
logan <huang1002788835@outlook.com>
2025-05-05 21:12:28 -0700

    338

--- src/main/java/com/dynamic_programming/CountBits_338.java
+++ src/main/java/com/dynamic_programming/CountBits_338.java
@@ -1,4 +1,14 @@
 package com.dynamic_programming;
 
 public class CountBits_338 {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++){
+            if (i % 2 == 0) dp[i] = dp[i / 2];
+            else dp[i] = dp[i - 1] + 1;
+        }
+
+        return dp;
+    }
 }
