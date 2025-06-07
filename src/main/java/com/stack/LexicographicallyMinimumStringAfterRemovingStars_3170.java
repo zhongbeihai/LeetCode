9840e48ad2d100d15cf9097f1cd526d6ecf55555
logan <huang1002788835@outlook.com>
2025-06-06 20:36:40 -0700

    3170

---
+++ src/main/java/com/stack/LexicographicallyMinimumStringAfterRemovingStars_3170.java
@@ -0,0 +1,34 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class LexicographicallyMinimumStringAfterRemovingStars_3170 {
+    public String clearStars(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        char[] ss = s.toCharArray();
+
+        int[] minCharCount = new int[26];
+
+        for (char c: ss){
+            if (c != '*') {
+                minCharCount[c - 'a']++;
+                sb.append(c);
+            }else {
+                int minChar = 0;
+                while (minChar < 26  && minCharCount[minChar] == 0){
+                    minChar++;
+                }
+                sb.deleteCharAt(sb.lastIndexOf(String.valueOf((char)(minChar + 'a'))));
+                minCharCount[minChar]--;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LexicographicallyMinimumStringAfterRemovingStars_3170 l = new LexicographicallyMinimumStringAfterRemovingStars_3170();
+        l.clearStars("aaba*");
+    }
+}
