289943927e71ff91215ed2938ca2bf973d04c19b
logan <huang1002788835@outlook.com>
2025-07-15 13:43:05 +1000

    3136

--- src/main/java/com/hashtable/FindSubsequenceOfLengthKWithLargestSum_2099.java
+++ src/main/java/com/hashtable/FindSubsequenceOfLengthKWithLargestSum_2099.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FindSubsequenceOfLengthKWithLargestSum_2099 {
     public int[] maxSubsequence(int[] nums, int k) {
@@ -20,6 +22,14 @@ public class FindSubsequenceOfLengthKWithLargestSum_2099 {
             result[i] = nums[indices[i][1]];
         }
         return result;
+    }
+
+    public boolean isValid(String word) {
+        String pattern = "^(?=.*[aeiouAEIOU])(?=.*[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z])[A-Za-z0-9]{3,}$";
+        Pattern reg = Pattern.compile(pattern);
+        Matcher matcher = reg.matcher(word);
+
+        return matcher.matches();
     }
 
     public static void main(String[] args) {
