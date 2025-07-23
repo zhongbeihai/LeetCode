1fec97d2905cb4341dc89700ba4dc32c7bfe19e0
logan <huang1002788835@outlook.com>
2025-07-23 11:07:42 +1000

    39

--- src/main/java/com/backtrack/CombinationSum_39.java
+++ src/main/java/com/backtrack/CombinationSum_39.java
@@ -16,29 +16,24 @@ public class CombinationSum_39 {
     private List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backTracking(candidates, target, new ArrayList<>(), 0, 0);
+        Arrays.sort(candidates);
+        backtrack(candidates, target, new ArrayList<>(), 0, 0);
 
         return res;
     }
 
-    public void backTracking(int[] candidates, int target, List<Integer> tem, int sum, int start){
-        if(sum == target){
-            res.add(new LinkedList<>(tem));
+    public void backtrack(int[] candidates, int target, List<Integer> tem, int curSum, int start){
+        if (curSum >= target){
+            if (curSum == target) res.add(new ArrayList<>(tem));
             return;
         }
 
-        if(sum > target){
-            return;
-        }
-
-        for(int i = start; i < candidates.length; i++){
+        for (int i = start; i < candidates.length; i++) {
             tem.add(candidates[i]);
-            sum += candidates[i];
 
-            backTracking(candidates, target, tem, sum, start);
+            backtrack(candidates, target, tem, curSum + candidates[i], i);
 
             tem.remove(tem.size() - 1);
-            sum -= candidates[i];
         }
     }
 
