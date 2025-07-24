eb6a5f9a1163aed56a988ee4f195642bc63a4670
logan <huang1002788835@outlook.com>
2025-07-24 17:02:51 +1000

    56

--- src/main/java/com/simulation/MergeIntervals_56.java
+++ src/main/java/com/simulation/MergeIntervals_56.java
@@ -3,14 +3,30 @@ package com.simulation;
 import com.sun.scenario.effect.Merge;
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MergeIntervals_56 {
     public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
 
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+        List<int[]> deque= new ArrayList<>();
+        deque.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++){
+            int[] last = deque.get(deque.size() - 1);
+
+            if (intervals[i][0] > last[1]){
+                deque.add(intervals[i]);
+            }else {
+                last[1] = Math.max(last[1], intervals[i][1]);
+            }
+        }
+
+
+        return deque.toArray(new int[deque.size()][]);
     }
 
     public static void main(String[] args) {
