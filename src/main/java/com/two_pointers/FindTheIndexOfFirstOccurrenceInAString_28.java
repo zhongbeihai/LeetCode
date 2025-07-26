d8511ea327ac0f91d4631c6fbd8686146d8365b4
logan <huang1002788835@outlook.com>
2025-07-26 15:53:04 +1000

    28

--- src/main/java/com/two_pointers/FindTheIndexOfFirstOccurrenceInAString_28.java
+++ src/main/java/com/two_pointers/FindTheIndexOfFirstOccurrenceInAString_28.java
@@ -2,16 +2,14 @@ package com.two_pointers;
 
 public class FindTheIndexOfFirstOccurrenceInAString_28 {
     public int strStr(String haystack, String needle) {
-        int p1 = 0, p2 = 0;
-
-        while (p1 < haystack.length()){
-            while (haystack.charAt(p1) == needle.charAt(p2)){
+        if (haystack.length() < needle.length()) return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            int p1 = i, p2= 0;
+            while (p1 < haystack.length() && haystack.charAt(p1) == needle.charAt(p2)){
                 p1++;
                 p2++;
-                if (p2 == needle.length()) return p1;
+                if (p2 == needle.length()) return i;
             }
-
-            p2 = p1;
         }
 
         return -1;
@@ -19,6 +17,7 @@ public class FindTheIndexOfFirstOccurrenceInAString_28 {
 
     public static void main(String[] args) {
         FindTheIndexOfFirstOccurrenceInAString_28 f = new FindTheIndexOfFirstOccurrenceInAString_28();
-        f.strStr("sadbutsad", "sad");
+        // f.strStr("mississippi", "issip");
+        f.strStr("ello", "ll");
     }
 }
