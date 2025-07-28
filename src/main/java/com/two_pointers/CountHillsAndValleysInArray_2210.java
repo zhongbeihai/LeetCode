0a2149644df9f4878c00bdd09162600e5f3f4601
logan <huang1002788835@outlook.com>
2025-07-28 21:21:43 +1000

    2210

--- src/main/java/com/two_pointers/CountHillsAndValleysInArray_2210.java
+++ src/main/java/com/two_pointers/CountHillsAndValleysInArray_2210.java
@@ -1,5 +1,23 @@
 package com.two_pointers;
 
 public class CountHillsAndValleysInArray_2210 {
+    public int countHillValley(int[] nums) {
+        int left = 0, count = 0;
 
+        for (int right = 1; right < nums.length - 1; right++){
+            if (nums[right] == nums[right + 1]) continue;
+            else {
+                if ( (nums[right] < nums[left] && nums[right] < nums[right + 1])
+                        || (nums[right] > nums[left] && nums[right] > nums[right + 1])) count++;
+            }
+            left = right;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountHillsAndValleysInArray_2210 c = new CountHillsAndValleysInArray_2210();
+        c.countHillValley(new int[]{6,6,5,5,4,1});
+    }
 }
