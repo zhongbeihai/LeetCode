037068a6b53029ecfaf7da240ec48d50ebc5e18c
logan <huang1002788835@outlook.com>
2025-07-29 19:23:11 +1000

    498

--- src/main/java/com/simulation/DiagonalTraverse_498.java
+++ src/main/java/com/simulation/DiagonalTraverse_498.java
@@ -1,4 +1,46 @@
 package com.simulation;
 
+import javax.jws.Oneway;
+
 public class DiagonalTraverse_498 {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int diagonalDir = 1;
+        int n = mat.length, m = mat[0].length, cnt = 0;
+        int[] res = new int[n * m];
+
+        int x = 0, y = 0;
+        while (cnt < n * m){
+            res[cnt++] = mat[x][y];
+
+            if (diagonalDir == 1){
+                if (x == 0){
+                    y++;
+                    diagonalDir *= -1;
+                } else if (y == m - 1) {
+                    x++;
+                    diagonalDir *= -1;
+                }else {
+                    x += diagonalDir;
+                    y += diagonalDir;
+                }
+            }else {
+                if (x == n - 1){
+                    y++;
+                    diagonalDir *= -1;
+                } else if (y == 0) {
+                    x++;
+                    diagonalDir *= -1;
+                }else {
+                    x += diagonalDir;
+                    y += diagonalDir;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
