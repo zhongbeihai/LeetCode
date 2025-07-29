4106a906d4bb837c1d9c8b372f0f0a0ee9213855
logan <huang1002788835@outlook.com>
2025-07-29 16:14:17 +1000

    34

--- src/main/java/com/search/FindTheFirstAndLastElementInSortedArray_34.java
+++ src/main/java/com/search/FindTheFirstAndLastElementInSortedArray_34.java
@@ -7,40 +7,40 @@ public class FindTheFirstAndLastElementInSortedArray_34 {
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[2];
         Arrays.fill(res, -1);
-        if(Arrays.stream(nums).noneMatch(e -> e == target)) return res;
-        int left = 0, right = nums.length - 1;
-        while (left < right - 1){
-            int mid = (left + right) / 2;
-            if(nums[mid] < target){
-                left = mid;
-            }else {
-                right = mid;
-            }
-        }
+        if (nums.length == 0) return res;
 
+        int rightBound = findFirstBiggerIndex(nums, target - 1);
+        if (rightBound == -1 || nums[rightBound] != target) return res;
+        else res[0] = rightBound;
+
+        while (rightBound < nums.length - 1 && nums[rightBound] == nums[rightBound + 1]) rightBound++;
+        res[1] = rightBound;
 
         return res;
     }
 
-    public int findFirstIndex(int[] nums, int target){
-        int l = 0, r = nums.length - 1;
-        int index = -1;
-        while (l <= r){
-            int mid = (l + r) / 2;
-            if(nums[mid] == target) index = mid;
-            if(nums[mid] < target){
-                l = mid + 1;
-            }else {
-                r = mid - 1;
+    public int findFirstBiggerIndex(int[] nums, int target){
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
-        return index;
+        return left < nums.length ? left : -1;
     }
+
+
 
     public static void main(String[] args) {
         FindTheFirstAndLastElementInSortedArray_34  f = new FindTheFirstAndLastElementInSortedArray_34();
-        //f.searchRange(new int[]{5,7,7,8,8,10}, 8);
+        // f.searchRange(new int[]{5,7,7,8,8,10}, 8);
         f.searchRange(new int[]{2,2},2);
+        //f.searchRange(new int[]{1}, 0);
     }
 }
