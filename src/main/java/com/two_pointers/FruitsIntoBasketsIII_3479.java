49e6017f43a24f4c2f102ef14602897a38d4f21d
logan <huang1002788835@outlook.com>
2025-08-07 00:01:54 +1000

    3479

--- src/main/java/com/two_pointers/FruitsIntoBasketsIII_3479.java
+++ src/main/java/com/two_pointers/FruitsIntoBasketsIII_3479.java
@@ -1,7 +1,25 @@
 package com.two_pointers;
 
+import java.util.TreeSet;
+
 public class FruitsIntoBasketsIII_3479 {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        TreeSet<Integer> available = new TreeSet<>();
+        for (int i = 0; i < baskets.length; i++) {
+            available.add(i);
+        }
 
+        int unplaced = fruits.length;
+        for (int fruit: fruits){
+            for (int idx: available){
+                if (baskets[idx] >= fruit){
+                    available.remove(idx);
+                    unplaced--;
+                    break;
+                }
+            }
+        }
+
+        return unplaced;
     }
 }
