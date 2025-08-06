73a5cd0e31039afc4bd4a811b95c17e412854533
logan <huang1002788835@outlook.com>
2025-08-07 00:14:57 +1000

    3479

--- src/main/java/com/two_pointers/FruitsIntoBasketsIII_3479.java
+++ src/main/java/com/two_pointers/FruitsIntoBasketsIII_3479.java
@@ -1,22 +1,30 @@
 package com.two_pointers;
 
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class FruitsIntoBasketsIII_3479 {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        TreeSet<Integer> available = new TreeSet<>();
-        for (int i = 0; i < baskets.length; i++) {
-            available.add(i);
+        int n = baskets.length;
+        TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(baskets[i], k -> new TreeSet<Integer>()).add(i);
         }
 
-        int unplaced = fruits.length;
+        int unplaced = 0;
         for (int fruit: fruits){
-            for (int idx: available){
-                if (baskets[idx] >= fruit){
-                    available.remove(idx);
-                    unplaced--;
-                    break;
-                }
+            Integer ceilingKey = map.ceilingKey(fruit);
+            if (ceilingKey == null){
+                unplaced++;
+                continue;
+            }
+
+            TreeSet<Integer> indices = map.get(ceilingKey);
+            int leftMost = indices.first();
+            indices.remove(leftMost);
+            if (indices.isEmpty()){
+                map.remove(ceilingKey);
             }
         }
 
