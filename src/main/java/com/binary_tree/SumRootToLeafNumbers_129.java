2f1afec7662e706efc75872e435bd9a111b7bc56
logan <huang1002788835@outlook.com>
2025-08-07 19:04:29 +1000

    129

--- src/main/java/com/binary_tree/SumRootToLeafNumbers_129.java
+++ src/main/java/com/binary_tree/SumRootToLeafNumbers_129.java
@@ -1,7 +1,33 @@
 package com.binary_tree;
 
-public class SumRootToLeafNumbers_129 {
-    public int sumNumbers(TreeNode root) {
+import structure.TreeNode;
 
+public class SumRootToLeafNumbers_129 {
+    private int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        dfs(root, new StringBuilder());
+
+        return sum;
     }
+
+    public void dfs(TreeNode root, StringBuilder sb){
+        sb.append(root.val);
+        if (root.left == null && root.right == null){
+            sum += Integer.parseInt(sb.toString());
+            return;
+        }
+
+        if (root.left != null) dfs(root.left, new StringBuilder(sb));
+        if (root.right != null) dfs(root.right, new StringBuilder(sb));
+    }
+
+    public static void main(String[] args) {
+        SumRootToLeafNumbers_129 s = new SumRootToLeafNumbers_129();
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(2);
+        t1.right = new TreeNode(3);
+
+        s.sumNumbers(t1);
+    }
+
 }
