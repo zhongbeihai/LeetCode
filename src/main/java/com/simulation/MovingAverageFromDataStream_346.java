9d32d550710c8363ea52175a79a2afe79f416a6b
logan <huang1002788835@outlook.com>
2025-08-07 18:02:38 +1000

    346

--- src/main/java/com/simulation/MovingAverageFromDataStream_346.java
+++ src/main/java/com/simulation/MovingAverageFromDataStream_346.java
@@ -1,4 +1,25 @@
 package com.simulation;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class MovingAverageFromDataStream_346 {
+    private Deque<Integer> deque;
+    private int curSum;
+    private int size;
+    public MovingAverageFromDataStream_346(int size) {
+        deque = new ArrayDeque<>();
+        curSum = 0;
+        this.size = size;
+    }
+
+    public double next(int val) {
+        deque.addLast(val);
+        curSum += val;
+        while (deque.size() > this.size){
+            curSum -= deque.pollFirst();
+        }
+
+        return (double) curSum / this.deque.size();
+    }
 }
