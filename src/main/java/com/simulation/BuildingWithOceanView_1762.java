d5aaa9f67b4fb51d14bb496bde26a5e165fd4253
logan <huang1002788835@outlook.com>
2025-08-07 14:04:07 +1000

    1762

--- src/main/java/com/simulation/BuildingWithOceanView_1762.java
+++ src/main/java/com/simulation/BuildingWithOceanView_1762.java
@@ -1,7 +1,31 @@
 package com.simulation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 public class BuildingWithOceanView_1762 {
     public int[] findBuildings(int[] heights) {
+        int n = heights.length;
+        List<Integer> res = new ArrayList<>();
 
+        int rightTallest = n - 1;
+        res.add(rightTallest);
+        for (int i = n - 2; i >= 0; i--){
+            if (heights[i] > heights[rightTallest]){
+                res.add(i);
+                rightTallest = i;
+            }
+        }
+
+        Collections.reverse(res);
+        return  res.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+
+    public static void main(String[] args) {
+        BuildingWithOceanView_1762 b = new BuildingWithOceanView_1762();
+        b.findBuildings(new int[]{4,3,2,1});
     }
 }
