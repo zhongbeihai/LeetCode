3b6f2d317acf6b0c6b9f336f48226bad6065c899
logan <huang1002788835@outlook.com>
2025-08-10 15:27:20 +1000

    165

--- src/main/java/com/simulation/CompareVersionNumber_165.java
+++ src/main/java/com/simulation/CompareVersionNumber_165.java
@@ -1,2 +1,21 @@
-package com.simulation;public class CompareVersionNumber_165 {
+package com.simulation;
+
+public class CompareVersionNumber_165 {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int len = Math.max(v1.length, v2.length);
+        for (int i = 0; i < len; i++){
+            int r1 = i >= v1.length ? 0 : Integer.parseInt(v1[i]);
+            int r2 = i >= v2.length ? 0 : Integer.parseInt(v2[i]);
+
+            if (r1 > r2) return 1;
+            else if (r1 < r2) {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
 }
