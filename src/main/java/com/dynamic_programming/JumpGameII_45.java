3ecb07d06ba7b2c0719e6cc33e42ba34b78a5036
logan <huang1002788835@outlook.com>
2025-08-19 03:00:56 +1000

    146

--- src/main/java/com/dynamic_programming/JumpGameII_45.java
+++ src/main/java/com/dynamic_programming/JumpGameII_45.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 public class JumpGameII_45 {
     public int jump(int[] nums) {
         // dp[i] is minimum number of jumps to i
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        int[] dp = new int[n];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
-        for (int i = 1; i < nums.length; i++){
-            int n = nums[i - 1];
-            for (int j = i; j < i + n ; j++){
-                dp[j] = Math.min(dp[j], dp[i - 1] + 1);
-                if(dp[nums.length - 1] != Integer.MAX_VALUE) return dp[nums.length - 1];
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (j + nums[j] >= i){
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
             }
-
         }
 
-        return dp[nums.length - 1];
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
         JumpGameII_45 jk = new JumpGameII_45();
-        jk.jump(new int[]{2,1});
+        jk.jump(new int[]{2,3,1,1,4});
     }
 }
--- src/main/java/com/simulation/LRUCache_146.java
+++ src/main/java/com/simulation/LRUCache_146.java
@@ -5,5 +5,72 @@ import java.util.Map;
 import java.util.Optional;
 
 public class LRUCache_146 {
+     static class Node{
+         int k, v;
+         Node prev, next;
+         Node(int key, int val){
+             k = key;
+             v = val;
+         }
+     }
 
+     private int capacity;
+     Node head = new Node(-1, -1);
+     Node tail = new Node(-1, -1);
+     Map<Integer, Node> map = new HashMap<>();
+     public LRUCache_146(int capacity){
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+        map = new HashMap<>();
+     }
+
+    public int get(int key) {
+        Node n = map.get(key);
+        if (n == null) return -1;
+
+        update(n);
+        return n.v;
+    }
+
+    public void put(int key, int value) {
+        Node n = map.get(key);
+        if (n != null){
+            n.v = value;
+            update(n);
+            return;
+        }
+        n = new Node(key, value);
+        map.put(key, n);
+        moveAfterHead(n);
+        if (map.size() > capacity){
+            Node p = pop();
+            map.remove(p.k);
+        }
+    }
+
+    public void update(Node x){
+         remove(x);
+         moveAfterHead(x);
+    }
+
+    public void moveAfterHead(Node x){
+         x.next = head.next;
+         x.prev = head;
+
+         head.next = x;
+         x.next.prev = x;
+    }
+
+    public void remove(Node x){
+         x.next.prev = x.prev;
+         x.prev.next = x.next;
+    }
+
+    public Node pop(){
+         Node n = tail.prev;
+
+         remove(n);
+         return n;
+    }
 }
