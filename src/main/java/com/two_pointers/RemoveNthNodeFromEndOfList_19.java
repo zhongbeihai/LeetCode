ecc32f6f411e317d1a7d621cf1c63ffb290e57b7
logan <huang1002788835@outlook.com>
2025-08-19 17:09:29 +1000

    19

--- src/main/java/com/two_pointers/RemoveNthNodeFromEndOfList_19.java
+++ src/main/java/com/two_pointers/RemoveNthNodeFromEndOfList_19.java
@@ -2,22 +2,25 @@ package com.two_pointers;
 
 import structure.ListNode;
 
+import java.util.List;
+
 public class RemoveNthNodeFromEndOfList_19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //ListNode dummy = new ListNode(-1, head);
-        ListNode fast = new ListNode(-1, head), slow = new ListNode(-1, head);
+        ListNode fast = new ListNode(-1, head);
+        ListNode slow = new ListNode(-1, head);
         while (n > 0){
+            if (fast == null) return head;
             fast = fast.next;
             n--;
         }
         while (fast.next != null){
-            fast = fast.next;
             slow = slow.next;
+            fast = fast.next;
         }
-        if (fast == head) return null;
-        if (slow.next == head) return fast;
-        slow.next = slow.next.next;
 
+        if (fast == head) return null;
+        if (slow.next == head) return slow.next.next;
+        slow.next = fast;
         return head;
     }
 }
