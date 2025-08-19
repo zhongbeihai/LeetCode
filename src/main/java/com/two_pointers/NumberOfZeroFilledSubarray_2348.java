7cb35001855c9c731b0ef15301b70585f6093ad8
logan <huang1002788835@outlook.com>
2025-08-19 17:24:50 +1000

    2348

--- src/main/java/com/two_pointers/NumberOfZeroFilledSubarray_2348.java
+++ src/main/java/com/two_pointers/NumberOfZeroFilledSubarray_2348.java
@@ -2,6 +2,16 @@ package com.two_pointers;
 
 public class NumberOfZeroFilledSubarray_2348 {
     public long zeroFilledSubarray(int[] nums) {
+        int left = 0, cnt = 0;
+        for (int right = 0; right < nums.length; right++){
+            if (nums[right] == 0){
+                cnt += right - left + 1;
+            }else {
+                left = right;
+                if (nums[left] != 0) left++;
+            }
+        }
 
+        return cnt;
     }
 }
