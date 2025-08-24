37e2d432f3e993e5d51e4d8506110aa9989d817c
logan <huang1002788835@outlook.com>
2025-08-24 13:44:23 +1000

    3

--- src/main/java/com/two_pointers/LongestSubstringWithoutRepeatingCharacters_3.java
+++ src/main/java/com/two_pointers/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -4,19 +4,20 @@ import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters_3 {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set = new HashSet<>();
-        int left = 0, maxLength = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
+        if (s.isEmpty()) return 0;
+        char[] ss = s.toCharArray();
+        int[] fre = new int[128];
+        int left = 0, max = 1;
+        for (int right = 0; right < ss.length; right++){
+            fre[ss[right]]++;
+            while (fre[ss[right]] > 1){
+                fre[ss[left]]--;
                 left++;
             }
-            set.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            max = Math.max(max, right - left + 1);
         }
 
-        return maxLength;
+        return max;
     }
 
     public static void main(String[] args) {
