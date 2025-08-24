6ed269d647ed418f26ac6e7a86f7278363e4e5c9
logan <huang1002788835@outlook.com>
2025-08-24 19:32:53 +1000

    79

--- src/main/java/com/search/WordSearch_79.java
+++ src/main/java/com/search/WordSearch_79.java
@@ -6,41 +6,39 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class WordSearch_79 {
-
     private int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
     public boolean exist(char[][] board, String word) {
+        int n = board.length, m = board[0].length;
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[0].length; j++){
                 if (board[i][j] !=  word.charAt(0)) continue;
-                if (dfs(board, word, new int[]{i, j})) return true;
+                if (dfs(board, i, j, word, 0, new boolean[n][m])) return true;
             }
         }
 
         return false;
     }
 
-    // 1 -> visiting, 2 -> visited
-    public boolean dfs(char[][] board, String word, int[] startPoint){
-        int x = startPoint[0], y = startPoint[1], p = startPoint[2];
-        if (p == word.length()) return true;
+    public boolean dfs(char[][] board, int x, int y, String word, int idx, boolean[][] used){
+        if (idx == word.length() - 1) return true;
+        int n = board.length, m = board[0].length;
 
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length
-                || board[x][y] != word.charAt(p)) return false;
+        used[x][y] = true;
+        for (int[] dir: dirs){
+            int nx = x + dir[0], ny = y + dir[1];
 
-        char tem = board[x][y];
-        board[x][y] = '#';
-
-        boolean found = dfs(board, word, new int[]{x + 1, y, p+ 1}) || // 下
-                dfs(board, word, new int[]{x-1, y, p + 1}) || // 上
-                dfs(board, word, new int[]{x, y + 1, p + 1}) || // 右
-                dfs(board, word, new int[]{x, y - 1, p + 1});
-
-        board[x][y] = tem;
-        return found;
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m
+                    && !used[nx][ny] && word.charAt(idx + 1) == board[nx][ny]){
+                if (dfs(board, nx, ny, word, idx + 1, used)) return true;
+            }
+        }
+        used[x][y] = false;
+        return false;
     }
 
     public static void main(String[] args) {
         WordSearch_79 w = new WordSearch_79();
-        w.exist(new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}}, "ABCCED");
+        //w.exist(new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}}, "ABCCED");
+        w.exist(new char[][]{{'C', 'A','A'}, {'A', 'A','A'}, {'B','C','D'}}, "AAB");
     }
 }
