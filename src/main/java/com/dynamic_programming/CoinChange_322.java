90fc7e7d21bee62d15a980df4414c19b9c370c8b
logan <huang1002788835@outlook.com>
2025-08-24 19:44:18 +1000

    322

--- src/main/java/com/dynamic_programming/CoinChange_322.java
+++ src/main/java/com/dynamic_programming/CoinChange_322.java
@@ -10,33 +10,22 @@ import java.util.Arrays;
  */
 class CoinChange_322 {
     public int coinChange(int[] coins, int amount){
-        // dp[amount]
-        // dp[i] = dp[i - coins[j]] + 1
-        if(amount == 0) return 0;
-
         Arrays.sort(coins);
-        if(amount < coins[0]) return -1;
-
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i: coins){
-            if(i <= amount) dp[i] = 1;
-        }
-
-        for(int i = 1; i <= amount; i++){
-            for(int coin: coins){
-                if((i - coin) >= 0 && dp[i - coin] != Integer.MAX_VALUE){
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
+        for (int i = 1; i <= amount; i++){
+            for (int coin: coins){
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
 
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
     }
 
     public static void main(String[] args) {
         CoinChange_322 c = new CoinChange_322();
-        c.coinChange(new int[]{1, 2147483447}, 2);
+        //c.coinChange(new int[]{1, 2147483447}, 2);
+        c.coinChange(new int[]{1,2,5}, 11);
     }
 }
