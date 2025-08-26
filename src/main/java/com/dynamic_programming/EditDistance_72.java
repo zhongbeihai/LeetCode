6ddf2149ab730ec1a14172c2f79938039316985f
logan <huang1002788835@outlook.com>
2025-08-26 22:13:11 +1000

    72

---
+++ src/main/java/com/dynamic_programming/EditDistance_72.java
@@ -0,0 +1,22 @@
+package com.dynamic_programming;
+
+public class EditDistance_72 {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+        int[][] dp = new int[n+ 1][m + 1];
+        for (int i = 0; i < m; i++) dp[i][0] = i;
+        for (int i = 0; i < n; i++) dp[0][i] = i;
+
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = 1 + Math.min(dp[i - 1][j - 1],  // replace
+                        Math.min(dp[i][j - 1], // delete
+                                dp[i - 1][j])); // insert
+            }
+        }
+
+        return dp[n][m];
+    }
+}
