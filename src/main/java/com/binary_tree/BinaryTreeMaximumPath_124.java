e8fbc36c8f48390ad0527eb83ec8931bb1f162ac
logan <huang1002788835@outlook.com>
2025-09-18 19:21:07 -0700

    124

--- src/main/java/com/binary_tree/BinaryTreeMaximumPath_124.java
+++ src/main/java/com/binary_tree/BinaryTreeMaximumPath_124.java
@@ -1,8 +1,23 @@
 package com.binary_tree;
 
+
 import structure.TreeNode;
-import sun.reflect.generics.tree.Tree;
 
 public class BinaryTreeMaximumPath_124 {
+    private int globalMax = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return globalMax;
+    }
 
+    public int helper(TreeNode root){
+        if (root == null) return 0;
+
+        int l = Math.max(helper(root.left), 0);
+        int r = Math.max(helper(root.right), 0);
+
+        globalMax = Math.max(globalMax, l + r + root.val);
+
+        return Math.max(l, r) + root.val;
+    }
 }
