dae65df3a67e2728a79771e8b5387d70e541337a
logan <huang1002788835@outlook.com>
2025-09-21 17:55:32 -0700

    3005

--- src/main/java/com/hashtable/CountElementsWithMaximumFrequency_3005.java
+++ src/main/java/com/hashtable/CountElementsWithMaximumFrequency_3005.java
@@ -1,14 +1,22 @@
 package com.hashtable;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CountElementsWithMaximumFrequency_3005 {
     public int maxFrequencyElements(int[] nums) {
         HashMap<Integer, Integer> fre = new HashMap<>();
-        int maxFre = 0;
+        int maxFre = 0, c = 0;
         for (int num: nums){
             fre.put(num, fre.getOrDefault(num, 0) + 1);
-            maxFre = Math.max(maxFre, fre.get(num));
+            if (fre.get(num) > maxFre){
+                maxFre = fre.get(num);
+                c = 1;
+            } else if (fre.get(num) == maxFre) {
+                c++;
+            }
         }
+
+        return maxFre * c;
     }
 }
