ae0f6b1ce831e397a7ab8102ecc3d78a8902bff7
logan <huang1002788835@outlook.com>
2025-09-22 20:48:48 -0700

    1209

---
+++ src/main/java/com/stack/RemoveAllAdjacentDuplicatesInStringII_1209.java
@@ -0,0 +1,38 @@
+package com.stack;
+
+import javafx.util.Pair;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInStringII_1209 {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+
+        for (char c: s.toCharArray()){
+            if (stack.isEmpty() ) {stack.push(new Pair<>(c, 1)); continue;}
+
+            Pair<Character, Integer> p = stack.peek();
+            if (c == p.getKey()){
+                if (p.getValue() == k - 1){
+                    for (int i = 0; i < k - 1; i++) stack.pop();
+                }else {
+                    stack.push(new Pair<>(c, p.getValue() + 1));
+                }
+            }else {
+                stack.push(new Pair<>(c, 1));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()){
+            sb.append(stack.pop().getKey());
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        RemoveAllAdjacentDuplicatesInStringII_1209 r = new RemoveAllAdjacentDuplicatesInStringII_1209();
+        r.removeDuplicates("abcd", 2);
+    }
+}
