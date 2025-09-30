4420baee8fbdf3aad7e20021cc625bcc6b98df1a
logan <huang1002788835@outlook.com>
2025-09-29 23:36:56 -0700

    Robot

---
+++ src/main/java/com/backtrack/Robot.java
@@ -0,0 +1,22 @@
+package com.backtrack;
+
+import java.util.HashSet;
+
+public class Robot {
+    HashSet<String> visited = new HashSet<>();
+    int[][] dirs = new int[][]{{1,0},{2}}
+    public void cleanRoom(Robot robot) {
+
+    }
+
+    private void clean(Robot robot, int x, int y){
+        visited.add("x" + "#" + "y");
+
+
+    }
+
+    private void turnLeft(){}
+    private void turnRight(){}
+    private void move(){}
+
+}
