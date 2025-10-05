e0595b0512f13e0ebd4b3a9c001813c5b17fb8e2
logan <huang1002788835@outlook.com>
2025-10-05 13:57:04 -0700

    33

--- src/main/java/com/binary_tree/SearchInRotatedSortedArray_33.java
+++ src/main/java/com/binary_tree/SearchInRotatedSortedArray_33.java
@@ -2,25 +2,34 @@ package com.binary_tree;
 
 public class SearchInRotatedSortedArray_33 {
     public int search(int[] nums, int target) {
-        int  n = nums.length, turningPoint = n;
-        int p = 0;
-        while (p + 1< n){
-            if (nums[p + 1] < nums[p]) {
-                turningPoint= p + 1;
-                break;
+        int n = nums.length;
+        int pivot = 0, right = n, model = nums[0];
+        while (pivot < right){
+            int mid = pivot + (right - pivot) / 2;
+            if (nums[mid] >= model) {
+                pivot = mid + 1;
+            }else {
+                right = mid;
             }
-            p++;
         }
 
-        int low = 0, high = n - 1;
-        if (target >= nums[0] && target <= nums[turningPoint - 1]) high = turningPoint - 1;
-        else low = turningPoint ;
+        int l, h;
+        if (pivot == n){
+            l = 0; h = n - 1;
+        } else if (target >= model) {
+            l = 0; h = pivot - 1;
+        }else {
+            l = pivot; h = n - 1;
+        }
 
-        while (low <= high){
-            int mid = (low + high) / 2;
+        while (l <= h){
+            int mid = l + (h - l) / 2;
             if (nums[mid] == target) return mid;
-            if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
+            if (nums[mid] < target){
+                l = mid + 1;
+            }else {
+                h = mid - 1;
+            }
         }
 
         return -1;
@@ -28,7 +37,7 @@ public class SearchInRotatedSortedArray_33 {
 
     public static void main(String[] args) {
         SearchInRotatedSortedArray_33 s= new SearchInRotatedSortedArray_33();
-        // s.search(new int[]{4,5,6,7,0,1,2}, -1);
-        s.search(new int[]{1}, 1);
+        // s.search(new int[]{4,5,6,7,0,1,2}, 0);
+         s.search(new int[]{1, 3}, 0);
     }
 }
