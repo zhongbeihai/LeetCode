b471ae5824e5d48f913097174cd20d54d6909403
logan <huang1002788835@outlook.com>
2025-10-04 17:07:06 -0700

    35

--- src/main/java/com/search/SearchInsertPosition_35.java
+++ src/main/java/com/search/SearchInsertPosition_35.java
@@ -4,7 +4,7 @@ public class SearchInsertPosition_35 {
     public int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length;
         while (left < right){
-            int mid = left + (right - left ) / 2;
+            int mid = left + (right - left ) / 2 ;
             if (nums[mid] >= target){
                 right = mid;
             }else {
