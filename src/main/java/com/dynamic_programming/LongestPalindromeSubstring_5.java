94593ef63c6d0e01264715f5940eb0ad9d1dba15
logan <huang1002788835@outlook.com>
2025-10-06 21:06:25 -0700

    5

--- src/main/java/com/dynamic_programming/LongestPalindromeSubstring_5.java
+++ src/main/java/com/dynamic_programming/LongestPalindromeSubstring_5.java
@@ -8,15 +8,16 @@ public class LongestPalindromeSubstring_5 {
         for (int i = 0; i < n; i++) {
             dp[i][i] = true;
         }
-        int start = 0, end = 0;
-        int maxLen = 0;
-        for (int i = n - 1; i >=0 ; i--){
-            for (int j = i + 1; j < n; j++){
-                if (ss[i] == ss[j] && (dp[i+1][j-1] || j - i < 2)){
-                    dp[i][j] = true;
-                    if (j - i + 1 > maxLen) {
-                        start = i; end = j;
-                        maxLen = j - i + 1;
+        int start = 0, end = 0, maxLength = 1;
+
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (ss[j] == ss[i] && (dp[j + 1][i - 1] || i - j < 3)) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLength) {
+                        start = j;
+                        end = i;
+                        maxLength = i - j + 1;
                     }
                 }
             }
