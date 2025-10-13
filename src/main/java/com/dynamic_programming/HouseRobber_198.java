50b4f62934c53be6e7099676332e0f2c2940b45f
logan <huang1002788835@outlook.com>
2025-10-13 13:17:39 -0700

    198

--- src/main/java/com/dynamic_programming/HouseRobber_198.java
+++ src/main/java/com/dynamic_programming/HouseRobber_198.java
@@ -2,22 +2,19 @@ package com.dynamic_programming;
 
 public class HouseRobber_198 {
     public int rob(int[] nums) {
-        // dp[i] is the largest money can get from 0 - ith
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        // dp[i] -> the largest money can get from house 0 to ith
+        int[] dp = new int[n];
         dp[0] = nums[0];
-        if(nums.length < 2) return dp[0];
-        dp[1] = nums[1];
-        if(nums.length < 3) return Math.max(dp[0], dp[1]);
 
-        for (int i = 2; i < nums.length; i++){
-            if(i - 3 >= 0){
-                dp[i] = Math.max(dp[i - 3],dp[i - 2] ) + nums[i];
-            }else {
-                dp[i] = dp[i - 2] + nums[i];
-            }
+        int res = 0;
+        for (int i = 1; i < n; i++){
+            int lastRobbed = i - 2 > 0 ? i - 2 : i;
+            dp[i] = Math.max(dp[lastRobbed] + nums[i], dp[i - 1]);
+            res = Math.max(res, dp[i]);
         }
 
-        return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
