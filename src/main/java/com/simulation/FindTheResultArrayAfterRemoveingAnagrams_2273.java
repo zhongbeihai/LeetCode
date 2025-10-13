b435557af22ddb3451ee99a2b9d05527f0da7374
logan <huang1002788835@outlook.com>
2025-10-12 21:15:43 -0700

    2273

--- src/main/java/com/simulation/FindTheResultArrayAfterRemoveingAnagrams_2273.java
+++ src/main/java/com/simulation/FindTheResultArrayAfterRemoveingAnagrams_2273.java
@@ -1,9 +1,34 @@
 package com.simulation;
 
-import java.util.List;
+import java.util.*;
 
 public class FindTheResultArrayAfterRemoveingAnagrams_2273 {
     public List<String> removeAnagrams(String[] words) {
+        int n = words.length;
+        int i = 0, j = i + 1;
 
+        List<String> res = new ArrayList<>();
+        while (i < n){
+            char[] word = words[i].toCharArray();
+            Arrays.sort(word);
+            String ana = String.valueOf(word);
+            res.add(words[i]);
+
+            while (j < n){
+                char[] wordJ = words[j].toCharArray();
+                Arrays.sort(wordJ);
+                if (ana.equals(String.valueOf(wordJ))) j++;
+                else break;
+            }
+            i = j;
+            j++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        FindTheResultArrayAfterRemoveingAnagrams_2273 f = new FindTheResultArrayAfterRemoveingAnagrams_2273();
+        f.removeAnagrams(new String[]{"abba","baba","bbaa","cd","cd"});
     }
 }
