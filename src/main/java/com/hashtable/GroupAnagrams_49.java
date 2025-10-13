f616e832452b1787910f63616aaff4261ebbb7f9
logan <huang1002788835@outlook.com>
2025-10-12 22:49:50 -0700

    49

--- src/main/java/com/hashtable/GroupAnagrams_49.java
+++ src/main/java/com/hashtable/GroupAnagrams_49.java
@@ -7,20 +7,12 @@ import java.util.List;
 
 public class GroupAnagrams_49 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // store the strings that are anagrams
         HashMap<String, List<String>> map = new HashMap<>();
-
         for (String str: strs){
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String newStr = new String(charArray);
-            if (map.get(newStr) == null){
-                List<String> l = new ArrayList<>();
-                l.add(str);
-                map.put(newStr, l);
-            }else {
-                map.get(newStr).add(str);
-            }
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String anagram = String.valueOf(c);
+            map.computeIfAbsent(anagram, v -> new ArrayList<>()).add(str);
         }
 
         return new ArrayList<>(map.values());
