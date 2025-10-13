633fbbf2ecce7d72fcec9b4c2e88d2eeaaa731a9
logan <huang1002788835@outlook.com>
2025-10-13 14:14:49 -0700

    222

--- src/main/java/com/binary_tree/CountCompleteTreeNodes_222.java
+++ src/main/java/com/binary_tree/CountCompleteTreeNodes_222.java
@@ -1,7 +1,20 @@
 package com.binary_tree;
 
+import structure.TreeNode;
+
+import java.util.EnumMap;
+
 public class CountCompleteTreeNodes_222 {
     public int countNodes(TreeNode root) {
+        return dfs(root);
+    }
 
+    public int dfs(TreeNode root){
+        if (root == null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        return left + right + 1;
     }
 }
