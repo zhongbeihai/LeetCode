4d3dd8424566b95729d079e5a951636589e06a1c
logan <huang1002788835@outlook.com>
2025-10-12 20:51:27 -0700

    2273

--- src/main/java/com/simulation/FindTheResultArrayAfterRemoveingAnagrams_2273.java
+++ src/main/java/com/simulation/FindTheResultArrayAfterRemoveingAnagrams_2273.java
@@ -1,4 +1,9 @@
 package com.simulation;
 
+import java.util.List;
+
 public class FindTheResultArrayAfterRemoveingAnagrams_2273 {
+    public List<String> removeAnagrams(String[] words) {
+
+    }
 }
