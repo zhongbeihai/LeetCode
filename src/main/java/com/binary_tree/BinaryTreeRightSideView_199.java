33a4cc1e1925c19a38859356bf1e7e37967bdde7
logan <huang1002788835@outlook.com>
2025-10-22 19:49:23 -0700

    199

--- src/main/java/com/binary_tree/BinaryTreeRightSideView_199.java
+++ src/main/java/com/binary_tree/BinaryTreeRightSideView_199.java
@@ -6,29 +6,20 @@ import java.util.*;
 
 public class BinaryTreeRightSideView_199 {
     public List<Integer> rightSideView(TreeNode root){
-        if(root == null) return new ArrayList<>();
+        if (root == null) return new ArrayList<>();
 
-        ArrayDeque<TreeNode> q = new ArrayDeque<TreeNode>();
-        ArrayList<Integer> list = new ArrayList<>();
-        q.addLast(root);
+        List<Integer> res = new ArrayList<>();
+        postOrder(root, 0, res);
 
-        while (!q.isEmpty()){
-            TreeNode lastNode = null;
-            // To store the number of nodes of each layer
-            int levelSize = q.size();
+        return res;
+    }
 
-            for(int i = 0; i < levelSize; i++){
-                TreeNode cur = q.poll();
-                lastNode = cur;
-                if(cur.left != null){
-                    q.addLast(cur.left);
-                }
-                if(cur.right != null){
-                    q.addLast(cur.right);
-                }
-            }
-            list.add(lastNode.val);
-        }
-        return list;
+    public void postOrder(TreeNode root, int level, List<Integer> res){
+        if (root == null) return;
+
+        if (level == res.size()) res.add(root.val);
+
+        postOrder(root.right, level + 1, res);
+        postOrder(root.left, level + 1, res);
     }
 }
