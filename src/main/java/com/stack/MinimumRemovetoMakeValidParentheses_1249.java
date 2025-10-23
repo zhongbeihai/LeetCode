1da0b88b37052042f0875e8430b73c53d42225ae
logan <huang1002788835@outlook.com>
2025-10-22 19:28:27 -0700

    1249

--- src/main/java/com/stack/MinimumRemovetoMakeValidParentheses_1249.java
+++ src/main/java/com/stack/MinimumRemovetoMakeValidParentheses_1249.java
@@ -5,27 +5,36 @@ import java.util.Stack;
 public class MinimumRemovetoMakeValidParentheses_1249 {
     public String minRemoveToMakeValid(String s) {
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z') continue;
-            else if(stack.isEmpty()) stack.push(i);
-            else if(s.charAt(i) == '(') stack.push(i);
-            else if(s.charAt(i) == ')'){
-                if (s.charAt(stack.peek()) == '('){
-                    stack.pop();
-                }else {
+        int leftParen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '('){
+                leftParen++;
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (leftParen > 0){
+                    leftParen--;
                     stack.push(i);
                 }
+            }else {
+                stack.push(i);
             }
         }
-        StringBuilder sb  = new StringBuilder(s);
-        while(!stack.isEmpty()){
-            sb.deleteCharAt(stack.pop());
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            if (leftParen > 0 && s.charAt(stack.peek()) == '(') {
+                stack.pop();
+                leftParen--;
+            }
+            else sb.append(s.charAt(stack.pop()));
         }
-        return sb.toString();
+
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
         MinimumRemovetoMakeValidParentheses_1249 m = new MinimumRemovetoMakeValidParentheses_1249();
-        m.minRemoveToMakeValid("))((");
+        m.minRemoveToMakeValid("))(((");
     }
 }
