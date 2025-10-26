5214ac1d1999afb0a28263b1a891e88102834063
logan <huang1002788835@outlook.com>
2025-10-25 18:31:28 -0700

    838

--- src/main/java/com/stack/PushDominoes_838.java
+++ src/main/java/com/stack/PushDominoes_838.java
@@ -1,4 +1,35 @@
 package com.stack;
 
 public class PushDominoes_838 {
+    public String pushDominoes(String dominoes) {
+        char[] dominoesArr = dominoes.toCharArray();
+        int left = 0;
+
+        for (int right = 1; right < dominoes.length(); right++){
+            if (dominoesArr[right] == '.') continue;
+            else if ((dominoesArr[left] == '.' && dominoesArr[right] == 'L')|| dominoesArr[left] == dominoesArr[right]) {
+                while (left < right) dominoesArr[left++] = dominoesArr[right];
+            } else if ((dominoesArr[left] == 'L' && dominoesArr[right] == 'R') ||
+                    (dominoesArr[left] == '.' && dominoesArr[right] == 'R') ) {
+                left = right;
+            } else if (dominoesArr[left] == 'R' && dominoesArr[right] == 'L') {
+                int r = right;
+                while (left < r){
+                    dominoesArr[left++] = 'R';
+                    dominoesArr[r--] = 'L';
+                }
+                left = right;
+            }
+        }
+        while (left + 1 < dominoesArr.length && dominoesArr[left] == 'R'){
+            dominoesArr[++left] = 'R';
+        }
+
+        return String.valueOf(dominoesArr);
+    }
+
+    public static void main(String[] args) {
+        PushDominoes_838 p = new PushDominoes_838();
+        p.pushDominoes(".R..L.");
+    }
 }
