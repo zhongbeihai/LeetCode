67db7401969e06313501f8b3e0451d87653230b1
logan <huang1002788835@outlook.com>
2025-10-29 14:00:48 -0700

    647

--- src/main/java/com/dynamic_programming/PalindromeSubstrings_647.java
+++ src/main/java/com/dynamic_programming/PalindromeSubstrings_647.java
@@ -3,23 +3,23 @@ package com.dynamic_programming;
 public class PalindromeSubstrings_647 {
     public int countSubstrings(String s) {
         char[] ss = s.toCharArray();
-        int n = ss.length;
-        boolean[][] dp = new boolean[n][n];
+        int n = ss.length, res = 0;
+        boolean[][] dp = new boolean[n][n]; // dp[i][j] -> is palindrome substring(i, j + 1)
         for (int i = 0; i < n; i++) {
             dp[i][i] = true;
+            res++;
         }
 
-        int cnt = n;
-        for (int i = n - 1; i >= 0 ;i--){
-            for (int j = i + 1; j < n; j++) {
-                if (ss[i] == ss[j] && (dp[i+1][j - 1] || j - i < 2)) {
+        for (int i = n - 2; i >= 0; i--){
+            for (int j = i + 1; j < n; j++){
+                if (ss[i] == ss[j] && (j - i == 1 || dp[i + 1][j - 1])){
                     dp[i][j] = true;
-                    cnt++;
+                    res++;
                 }
             }
         }
 
-        return cnt;
+        return res;
     }
 
     public static void main(String[] args) {
