ecdad7648c298bb1a5083ab0ce13b8296c70e734
logan <huang1002788835@outlook.com>
2025-10-30 14:27:15 -0700

    1526

--- src/main/java/com/dynamic_programming/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray_1526.java
+++ src/main/java/com/dynamic_programming/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray_1526.java
@@ -2,15 +2,17 @@ package com.dynamic_programming;
 
 public class MinimumNumberOfIncrementsOnSubarraysToFormATargetArray_1526 {
     public int minNumberOperations(int[] target) {
-        int[] dp = new int[target.length];
+        int n = target.length;
+        int[] dp = new int[n];
+        dp[0] = target[0];
 
-        dp[target.length - 1] = target[target.length - 1];
-        for(int i = target.length - 2; i >= 0; i--){
-            if(target[i] <= target[i + 1]) dp[i] = dp[i + 1];
-            else dp[i] = dp[i + 1] + target[i] - target[i + 1];
+        for (int i = 1; i < n; i++){
+            if (target[i] > target[i - 1])
+                dp[i] = dp[i - 1] + target[i] - target[i - 1];
+            else dp[i] = dp[i - 1];
         }
 
-        return dp[0];
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
