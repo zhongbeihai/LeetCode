fe3fd6f3e3e5719bcb32f790726706b2dc389efe
logan <huang1002788835@outlook.com>
2025-11-03 14:07:37 -0800

    14

--- src/main/java/com/simulation/LongestCommonPrefix_14.java
+++ src/main/java/com/simulation/LongestCommonPrefix_14.java
@@ -4,17 +4,14 @@ public class LongestCommonPrefix_14 {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null) return "";
 
-        String ori = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < ori.length() && j < strs[i].length(); j++) {
-                if (ori.charAt(j) == strs[i].charAt(j)) sb.append(ori.charAt(j));
-                else break;
-            }
-            ori = sb.toString();
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++){
+            int p = 0;
+            while (p < prefix.length() && p < strs[i].length() && prefix.charAt(p) == strs[i].charAt(p)) p++;
+            prefix = prefix.substring(0, p);
         }
 
-        return ori;
+        return prefix;
     }
 
     public static void main(String[] args) {
