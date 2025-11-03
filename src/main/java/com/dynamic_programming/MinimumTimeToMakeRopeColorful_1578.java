d83c12fe7fdaab97c7d439be47ea9f75a6188723
logan <huang1002788835@outlook.com>
2025-11-02 18:17:13 -0800

    1578

--- src/main/java/com/dynamic_programming/MinimumTimeToMakeRopeColorful_1578.java
+++ src/main/java/com/dynamic_programming/MinimumTimeToMakeRopeColorful_1578.java
@@ -5,30 +5,22 @@ import java.util.Stack;
 
 public class MinimumTimeToMakeRopeColorful_1578 {
     public int minCost(String colors, int[] neededTime) {
-        Stack<Integer> stack = new Stack<>();
-        int sum = 0;
-        stack.push(0);
-        for(int i = 1; i < colors.length(); i++){
-            int head = stack.pop();
-            if(colors.charAt(head) != colors.charAt(i)){
-                stack.push(head);
-                stack.push(i);
-            }else {
-                if(neededTime[head] > neededTime[i]){
-                    sum += neededTime[i];
-                    stack.push(head);
-                }else {
-                    sum += neededTime[head];
-                    stack.push(i);
-                }
+        int n = colors.length();
+        int[] dp = new int[n];
+
+        for (int i = 1; i < n; i++){
+            if (colors.charAt(i) != colors.charAt(i - 1)) dp[i] = dp[i - 1];
+            else {
+                dp[i] = dp[i - 1] + Math.min(neededTime[i], neededTime[i - 1]);
+                neededTime[i] = Math.max(neededTime[i - 1], neededTime[i]);
             }
         }
 
-        return sum;
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
         MinimumTimeToMakeRopeColorful_1578 m = new MinimumTimeToMakeRopeColorful_1578();
-        m.minCost("bbbaaa", new int[]{4,9,3,8,8,9});
+        m.minCost("aaabbbabbbb", new int[]{3,5,10,7,5,3,5,5,4,8,1});
     }
 }
