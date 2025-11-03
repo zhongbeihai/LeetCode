d74f30a8908c1379b3d1bd42fd35ef6f6239399a
logan <huang1002788835@outlook.com>
2025-11-03 14:21:39 -0800

    875

--- src/main/java/com/search/KokoEatingBananas_875.java
+++ src/main/java/com/search/KokoEatingBananas_875.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class KokoEatingBananas_875 {
     public int minEatingSpeed(int[] piles, int h) {
         int n = piles.length;
-        int low = 1, high = n == 1 ? piles[0] : piles[n - 1];
+        Arrays.sort(piles);
+        if (h == piles.length) return piles[n - 1];
+        long low = 1, high = piles[n - 1];
         while (low < high){
-            int mid = low + (high - low) / 2;
+            long mid = low + (high - low) / 2;
             if (isValid(piles, mid, h)){
                 high = mid;
             }else {
@@ -15,25 +17,26 @@ public class KokoEatingBananas_875 {
             }
         }
 
-        return low;
+        return (int) low;
     }
 
-    public boolean isValid(int[] piles, int k, int h){
-        int n = piles.length;
-        for (int p : piles) {
-            // ceil(p / k) using integers
-            h -= (p + k - 1) / k;
-            if (h < 0) return false; // early exit
+    public boolean isValid(int[] piles, long mid, int h){
+        double cnt = 0;
+        for(int pile: piles){
+            cnt += Math.ceil((double) pile / mid);
+            if (cnt > h) return false;
         }
 
-        return true;
+        return cnt <= h;
     }
+
 
     public static void main(String[] args) {
         KokoEatingBananas_875 k = new KokoEatingBananas_875();
         // k.minEatingSpeed(new int[]{805306368,805306368,805306368}, 1000000000);
         // k.isValid(new int[]{805306368,805306368,805306368}, 1000000000, 2);
         // k.minEatingSpeed(new int[]{3,6,7,11}, 8);
-        k.minEatingSpeed(new int[]{312884470}, 312884469);
+        // k.minEatingSpeed(new int[]{312884470}, 312884469);
+        k.minEatingSpeed(new int[]{30,11,23,4,20}, 6);
     }
 }
