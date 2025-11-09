7e677c1dd33c96c1647e6d3a61a991c47a2aa1e8
logan <huang1002788835@outlook.com>
2025-11-09 15:07:42 -0800

    79

--- src/main/java/com/search/WordSearch_79.java
+++ src/main/java/com/search/WordSearch_79.java
@@ -8,33 +8,37 @@ import java.util.Queue;
 public class WordSearch_79 {
     private int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
     public boolean exist(char[][] board, String word) {
-        int n = board.length, m = board[0].length;
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[0].length; j++){
-                if (board[i][j] !=  word.charAt(0)) continue;
-                if (dfs(board, i, j, word, 0, new boolean[n][m])) return true;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)){
+                    if (dfs(board, word, 0, new boolean[m][n], i, j)) return true;
+                }
             }
         }
 
         return false;
     }
 
-    public boolean dfs(char[][] board, int x, int y, String word, int idx, boolean[][] used){
+    public boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int x, int y){
+        int m = board.length, n = board[0].length;
         if (idx == word.length() - 1) return true;
-        int n = board.length, m = board[0].length;
+        visited[x][y] =true;
 
-        used[x][y] = true;
         for (int[] dir: dirs){
             int nx = x + dir[0], ny = y + dir[1];
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m
-                    && !used[nx][ny] && word.charAt(idx + 1) == board[nx][ny]){
-                if (dfs(board, nx, ny, word, idx + 1, used)) return true;
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n
+                    && word.charAt(idx + 1) == board[nx][ny]
+                    && !visited[nx][ny]){
+                if (dfs(board, word, idx + 1, visited, nx, ny)) return true;
             }
         }
-        used[x][y] = false;
+
+        visited[x][y] = false;
         return false;
     }
+
 
     public static void main(String[] args) {
         WordSearch_79 w = new WordSearch_79();
