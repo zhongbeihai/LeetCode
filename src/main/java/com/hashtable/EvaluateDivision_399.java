4368e44fbdf56e40f43662910054ccaff99bb9b9
logan <huang1002788835@outlook.com>
2025-11-13 23:37:29 -0800

    399

--- src/main/java/com/hashtable/EvaluateDivision_399.java
+++ src/main/java/com/hashtable/EvaluateDivision_399.java
@@ -5,39 +5,41 @@ import java.util.*;
 public class EvaluateDivision_399 {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         HashMap<String, HashMap<String, Double>> map = new HashMap<>();
-        HashSet<String> inputSet = new HashSet<>();
-        int i = 0;
-        for (List<String> equation: equations){
-            double val = values[i++];
-            map.computeIfAbsent(equation.get(0), k -> new HashMap<>()).put(equation.get(1), val);
-            map.computeIfAbsent(equation.get(1), k -> new HashMap<>()).put(equation.get(0), 1.0 / val);
-            inputSet.add(equation.get(0));
-            inputSet.add(equation.get(1));
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> list = equations.get(i);
+            map.computeIfAbsent(list.get(0), k -> new HashMap<>()).put(list.get(1), values[i]);
+            map.computeIfAbsent(list.get(1), k -> new HashMap<>()).put(list.get(0), 1.0 / values[i]);
         }
 
         double[] res = new double[queries.size()];
-        for (int j = 0; j < queries.size(); j++) {
-            List<String> query = queries.get(j);
-            if (!inputSet.containsAll(query)) res[j] = -1.0;
-            else if (query.get(0).equals(query.get(1))) {
-                res[j] = 1.0;
-            } else res[j] = dfs(query.get(0), query.get(1), 1.0, map, new HashSet<>());
+        int i = 0;
+        for (List<String > query: queries){
+            res[i++] = dfs(map, query.get(0), query.get(1), new HashSet<>());
         }
 
         return res;
+
     }
 
-    public double dfs(String start, String end, double curValue, HashMap<String, HashMap<String, Double>> map, Set<String> visited){
-        if (!map.containsKey(start) || visited.contains(start)) return -1.0;
-        if (map.get(start).containsKey(end)) return curValue * map.get(start).get(end);
+    public double dfs(HashMap<String, HashMap<String, Double>> map, String start, String end, Set<String> visited){
+        if (!map.containsKey(start)) return -1.0;
+        if (start.equals(end)) return 1.0;
 
         visited.add(start);
-        for (Map.Entry<String, Double> entry : map.get(start).entrySet()) {
-            double result = dfs(entry.getKey(), end, curValue * entry.getValue(), map, visited);
-            if (result != -1.0) return result;
+        HashMap<String, Double> nexts = map.get(start);
+        if (nexts == null) return  -1;
+
+        for (String next: nexts.keySet()){
+            if (Objects.equals(next, end)) return nexts.get(next);
+            if (!visited.contains(next)) {
+                double v = nexts.get(next) * dfs(map, next, end, visited);
+                if (v > 0) return v;
+            }
         }
-        return -1.0;
+
+        return -1;
     }
+
 
     public static void main(String[] args) {
 
