f2123393bd1c644fd2f10f8b8c786c81bf84e6b4
logan <huang1002788835@outlook.com>
2025-11-17 18:40:16 -0800

    84

---
+++ src/main/java/com/stack/LargestRectangleInHistogram_84.java
@@ -0,0 +1,37 @@
+package com.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class LargestRectangleInHistogram_84 {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                int idx = stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int width = i - left - 1;
+                max = Math.max(max, width * heights[idx]);
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            int left = stack.isEmpty() ? -1 : stack.peek();
+            int width = n - left - 1;
+            max = Math.max(max, width * heights[idx]);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LargestRectangleInHistogram_84 l = new LargestRectangleInHistogram_84();
+        // l.largestRectangleArea(new int[]{2,1,5,6,2,3});
+        l.largestRectangleArea(new int[]{2, 4});
+    }
+}
