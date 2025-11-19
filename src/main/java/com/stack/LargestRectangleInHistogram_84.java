c6a0ac1815f6737687704143434424844722e80c
logan <huang1002788835@outlook.com>
2025-11-18 21:28:25 -0800

    84

--- src/main/java/com/stack/LargestRectangleInHistogram_84.java
+++ src/main/java/com/stack/LargestRectangleInHistogram_84.java
@@ -7,19 +7,18 @@ public class LargestRectangleInHistogram_84 {
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
         int max = 0;
-        int n = heights.length;
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]){
                 int idx = stack.pop();
-                int left = stack.isEmpty() ? -1 : stack.peek();
+                int left = stack.isEmpty() ? -1 : stack.peek() ;
                 int width = i - left - 1;
                 max = Math.max(max, width * heights[idx]);
             }
             stack.push(i);
         }
 
-        while (!stack.isEmpty()) {
+        int n = heights.length;
+        while (!stack.isEmpty()){
             int idx = stack.pop();
             int left = stack.isEmpty() ? -1 : stack.peek();
             int width = n - left - 1;
@@ -32,6 +31,6 @@ public class LargestRectangleInHistogram_84 {
     public static void main(String[] args) {
         LargestRectangleInHistogram_84 l = new LargestRectangleInHistogram_84();
         // l.largestRectangleArea(new int[]{2,1,5,6,2,3});
-        l.largestRectangleArea(new int[]{2, 4});
+        l.largestRectangleArea(new int[]{2, 1, 2});
     }
 }
