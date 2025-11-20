19e8d8be3e0e8b11f6b5f1bf1fbf2e745836abb8
logan <huang1002788835@outlook.com>
2025-11-19 21:05:48 -0800

    653

---
+++ src/main/java/com/binary_tree/TwoSumIV_InputIsABST_653.java
@@ -0,0 +1,21 @@
+package com.binary_tree;
+
+import structure.TreeNode;
+
+import java.util.HashSet;
+
+public class TwoSumIV_InputIsABST_653 {
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, set, k);
+    }
+
+    public boolean dfs(TreeNode root, HashSet<Integer> set, int k){
+        if (root == null) return false;
+
+        if (set.contains(k - root.val)) return true;
+        set.add(root.val);
+
+        return dfs(root.left, set, k) | dfs(root.right, set, k);
+    }
+}
