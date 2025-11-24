1975e3e24e7a6f12c5e43cc1d1e9d0d3bed9156c
logan <huang1002788835@outlook.com>
2025-11-23 20:53:53 -0800

    57

--- src/main/java/com/simulation/InsertInterval_57.java
+++ src/main/java/com/simulation/InsertInterval_57.java
@@ -8,36 +8,32 @@ public class InsertInterval_57 {
         if (intervals.length == 0) return new int[][]{newInterval};
         if (newInterval.length == 0) return intervals;
 
-        int n = intervals.length;
-        List<int[]> list = new ArrayList<>();
-
-        int i = 0;
-        while (i < n && intervals[i][1] < newInterval[0]){
-            list.add(intervals[i++]);
-        }
+        List<int[]> res = new ArrayList<>();
+        int i = 0, n = intervals.length;
+        while (i < n && intervals[i][1] < newInterval[0]) res.add(intervals[i++]);
         int start = newInterval[0], end = newInterval[1];
-        while (i < n && intervals[i][0] <= end){
+        while (i < n && (newInterval[0] <= intervals[i][1] && newInterval[1] >= intervals[i][0])){
             start = Math.min(start, intervals[i][0]);
             end = Math.max(end, intervals[i][1]);
             i++;
         }
-        list.add(new int[]{start, end});
-        while (i < n){
-            list.add(intervals[i++]);
+
+        res.add(new int[]{start, end});
+
+        while (i < intervals.length){
+            res.add(intervals[i++]);
         }
 
-        int[][] res = new int[list.size()][2];
-        for (i = 0; i < list.size(); i++) {
-            res[i] = list.get(i);
+        int[][] r = new int[res.size()][];
+        for (int j = 0; j < res.size(); j++) {
+            r[j] = res.get(j);
         }
-
-        return res;
-
+        return r;
     }
 
     public static void main(String[] args) {
         InsertInterval_57 i = new InsertInterval_57();
-        // i.insert(new int[][]{{1,2},{3,5},{6,7},{8,10},{12,16}}, new int[]{4,8});
+        i.insert(new int[][]{{1,2},{3,5},{6,7},{8,10},{12,16}}, new int[]{4,8});
         // i.insert(new int[][]{{1,3},{6,9}}, new int[]{2,5});
         i.insert(new int[][]{{1,5}}, new int[]{6,8});
     }
