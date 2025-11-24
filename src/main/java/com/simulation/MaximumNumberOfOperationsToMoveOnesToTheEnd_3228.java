42db5485517dc1eb43796b29e557f612888b4171
logan <huang1002788835@outlook.com>
2025-11-24 15:26:58 -0800

    3228

--- src/main/java/com/simulation/MaximumNumberOfOperationsToMoveOnesToTheEnd_3228.java
+++ src/main/java/com/simulation/MaximumNumberOfOperationsToMoveOnesToTheEnd_3228.java
@@ -5,17 +5,20 @@ public class MaximumNumberOfOperationsToMoveOnesToTheEnd_3228 {
         int ones = 0;
 
         int res = 0;
+        char prev = s.charAt(0);
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 1) ones++;
-            else {
+            if (s.charAt(i) == '1') ones++;
+            else if (s.charAt(i) == '0' && prev == '1'){
                 res += ones;
             }
+            prev = s.charAt(i);
         }
 
         return res;
     }
 
     public static void main(String[] args) {
-
+        MaximumNumberOfOperationsToMoveOnesToTheEnd_3228 m = new MaximumNumberOfOperationsToMoveOnesToTheEnd_3228();
+        m.maxOperations("1001101");
     }
 }
