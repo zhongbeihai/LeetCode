bc7317bb90cb0e66df8848752bad769179542310
Logan <huang1002788835@outlook.com>
2026-01-08 23:43:31 -0800

    79

--- src/main/java/com/search/WordSearch_79.java
+++ src/main/java/com/search/WordSearch_79.java
@@ -8,13 +8,10 @@ import java.util.Queue;
 public class WordSearch_79 {
     private int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)){
-                    if (dfs(board, word, 0, new boolean[m][n], i, j)) return true;
-                }
+        int n = board.length, m = board[0].length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (board[i][j] == word.charAt(0) && dfs(board, word, 0, new boolean[n][m], i, j)) return true;
             }
         }
 
@@ -22,16 +19,14 @@ public class WordSearch_79 {
     }
 
     public boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int x, int y){
-        int m = board.length, n = board[0].length;
-        if (idx == word.length() - 1) return true;
-        visited[x][y] =true;
+        int n = board.length, m = board[0].length;
 
+        if (idx == word.length() - 1) return true;
+        visited[x][y] = true;
         for (int[] dir: dirs){
             int nx = x + dir[0], ny = y + dir[1];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n
-                    && word.charAt(idx + 1) == board[nx][ny]
-                    && !visited[nx][ny]){
-                if (dfs(board, word, idx + 1, visited, nx, ny)) return true;
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == word.charAt(idx + 1) && !visited[nx][ny]){
+                if( dfs(board, word, idx + 1, visited, nx, ny)) return true;
             }
         }
 
