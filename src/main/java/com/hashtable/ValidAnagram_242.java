73eed4cb7a7dbee2191167cbca70bffa017a291a
Logan <huang1002788835@outlook.com>
2026-01-09 20:27:21 -0800

    242

--- src/main/java/com/hashtable/ValidAnagram_242.java
+++ src/main/java/com/hashtable/ValidAnagram_242.java
@@ -11,22 +11,22 @@ import java.util.stream.IntStream;
  */
 public class ValidAnagram_242 {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        char[] ss = s.toCharArray();
-        char[] tt = t.toCharArray();
-
         HashMap<Character, Integer> map = new HashMap<>();
-        IntStream.range(0, ss.length).forEach(i -> {
-            map.put(ss[i], map.getOrDefault(ss[i], 0) + 1);
-            map.put(tt[i], map.getOrDefault(tt[i], 0) - 1);
-        });
-
-        for(Map.Entry<Character, Integer> e : map.entrySet()){
-            if (e.getValue() != 0 )
-                return false;
+        for (char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        return true;
+
+        for (char c: t.toCharArray()){
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) map.remove(c);
+        }
+
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram_242 v = new ValidAnagram_242();
+        v.isAnagram("anagram", "nagaram");
     }
 }
