fc549410128e2843011ac56eb368b7d83f212845
Logan <huang1002788835@outlook.com>
2026-01-11 16:34:55 -0800

    152

--- src/main/java/com/backtrack/Subsets_78.java
+++ src/main/java/com/backtrack/Subsets_78.java
@@ -1,10 +1,13 @@
 package com.backtrack;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Subsets_78 {
     private List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
         backtrack(nums, 0, new ArrayList<>());
 
@@ -12,12 +15,10 @@ public class Subsets_78 {
     }
 
     public void backtrack(int[] nums, int idx, List<Integer> tem){
-        // stop condition
         res.add(new ArrayList<>(tem));
-        if (idx >= nums.length) return;
+        if (tem.size() == nums.length) return;
 
-
-        for (int i = idx; i < nums.length; i++){
+        for (int i = idx; i < nums.length; i++) {
             tem.add(nums[i]);
 
             backtrack(nums, i + 1, tem);
@@ -25,6 +26,8 @@ public class Subsets_78 {
             tem.remove(tem.size() - 1);
         }
     }
+
+
 
     public static void main(String[] args) {
         Subsets_78 s = new Subsets_78();
