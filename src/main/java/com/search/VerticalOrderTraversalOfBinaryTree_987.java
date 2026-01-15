c2475e84b1ac6ad8756d84473e3b97edc1de7b7c
Logan Wong <huang1002788835@outlook.com>
2026-01-14 16:05:01 -0800

    987

--- src/main/java/com/search/VerticalOrderTraversalOfBinaryTree_987.java
+++ src/main/java/com/search/VerticalOrderTraversalOfBinaryTree_987.java
@@ -6,35 +6,34 @@ import java.util.*;
 import java.util.concurrent.ConcurrentMap;
 
 public class VerticalOrderTraversalOfBinaryTree_987 {
-    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-
+    private Map<Integer, Map<Integer, PriorityQueue<Integer>>> map = new TreeMap<>(); // <col, <level, Que>>
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         dfs(root, 0, 0);
 
         List<List<Integer>> res = new ArrayList<>();
-        for (int col : map.keySet()) {
-            List<Integer> cur = new ArrayList<>();
-            for (int row : map.get(col).keySet()) {
-                PriorityQueue<Integer> pq = map.get(col).get(row);
-                while (!pq.isEmpty()) {
-                    cur.add(pq.poll());
+        for (Integer col: map.keySet()){
+            List<Integer> colList = new ArrayList<>();
+            Map<Integer, PriorityQueue<Integer>> colMap = map.get(col);
+            for (Integer row: map.get(col).keySet()){
+                PriorityQueue<Integer> pq = colMap.get(row);
+                while (!pq.isEmpty()){
+                    colList.add(pq.poll());
                 }
             }
 
-            res.add(cur);
+            res.add(colList);
         }
 
         return res;
     }
 
-    public void dfs(TreeNode root, int row, int col) {
+    public void dfs(TreeNode root, int col, int level){
         if (root == null) return;
 
-        map.putIfAbsent(col, new TreeMap<>());
-        map.get(col).putIfAbsent(row, new PriorityQueue<>());
-        map.get(col).get(row).offer(root.val);
+        map.computeIfAbsent(col, v -> new TreeMap<>())
+                .computeIfAbsent(level, v -> new PriorityQueue<>()).add(root.val);
 
-        dfs(root.left, row + 1, col - 1);
-        dfs(root.right, row + 1, col + 1);
+        dfs(root.left, col - 1, level + 1); // to left child
+        dfs(root.right, col + 1, level + 1); // to right child;
     }
 }
