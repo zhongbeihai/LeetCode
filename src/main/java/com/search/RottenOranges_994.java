a5d78a90097c3d3e0ff4190c8e940ce6d7a13c98
Logan Wong <huang1002788835@outlook.com>
2026-01-15 23:46:11 -0800

    994

--- src/main/java/com/search/RottenOranges_994.java
+++ src/main/java/com/search/RottenOranges_994.java
@@ -7,38 +7,35 @@ import java.util.*;
 
 public class RottenOranges_994 {
     public int orangesRotting(int[][] grid) {
-        PriorityQueue<int[]> rotten = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(i -> i[0])); // (time, x, y)
         int m = grid.length, n = grid[0].length;
-        int[][] visited = new int[m][n];
-        int count = 0, fresh = 0;
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (grid[i][j] == 2) {
-                    rotten.add(new int[]{i, j, 1});
-                    visited[i][j] = 1;
-                }
-                if (grid[i][j] == 1) fresh++;
+
+        int cntOranges = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2 || grid[i][j] == 1) cntOranges++;
+                if (grid[i][j] == 2)pq.add(new int[]{0, i, j});
             }
         }
-        int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-        while (!rotten.isEmpty()){
-            int[] p = rotten.poll();
-            int x = p[0], y = p[1], min = p[2];
-            if (visited[x][y] == 0) fresh--;
-            visited[x][y] = min;
-            count = Math.max(count, min - 1);
-            for (int[] dir: dirs){
-                int nx = x + dir[0], ny = y + dir[1], nm = min + 1;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n
-                        && grid[nx][ny] == 1 &&
-                        (visited[nx][ny] > nm || visited[nx][ny] == 0)){
-                    rotten.add(new int[]{nx, ny, nm});
+
+        int[][] dirs = new int[][]{{1,0}, {0,1}, {-1, 0}, {0, -1}};
+        // bfs
+        int maxTime = 0;
+        while (!pq.isEmpty()){
+            int[] curO = pq.poll();
+            int t = curO[0], x = curO[1], y = curO[2];
+            maxTime = Math.max(maxTime, curO[0]);
+            cntOranges--;
+            for(int[] dir: dirs){
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
+                    pq.add(new int[]{t + 1, nx, ny});
+                    grid[nx][ny] = 2;
                 }
             }
         }
 
-        if (fresh != 0) return -1;
-        return count;
+        return cntOranges == 0 ? maxTime : -1;
     }
 
     public static void main(String[] args) {
