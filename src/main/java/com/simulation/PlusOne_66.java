766a644fb1fa7b73908646b3d935cf147115028a
Logan Wong <huang1002788835@outlook.com>
2026-01-21 00:33:21 -0800

    66

--- src/main/java/com/simulation/PlusOne_66.java
+++ src/main/java/com/simulation/PlusOne_66.java
@@ -4,26 +4,29 @@ import java.util.Arrays;
 
 public class PlusOne_66 {
     public int[] plusOne(int[] digits) {
-        recursion(digits, digits.length - 1);
+        int n = digits.length;
 
-        if (digits[0] == 0){
-            int[] n = new int[digits.length + 1];
-            n[0] = 1;
-            for (int i = 1; i < digits.length; i++){
-                n[i] = digits[i - 1];
-            }
-            return n;
+        int carry = digits[n - 1] + 1 == 10 ? 1 : 0;
+        digits[n - 1] = (digits[n - 1] + 1) % 10;
+        for (int i = n - 2; i >= 0; i--){
+            int d = (digits[i] + carry) % 10;
+            carry = digits[i] + carry == 10 ? 1 : 0;
+            digits[i] = d;
         }
-        return digits;
+
+        if (digits[0] == 0 && carry == 1){
+            int[] res = new int[n + 1];
+            res[0] = 1;
+            System.arraycopy(digits, 0, res, 1, n - 1);
+            return res;
+        }else {
+            return digits;
+        }
     }
 
-    public void recursion(int[] digits, int pos){
-        digits[pos] = (digits[pos] + 1) % 10;
-        if (digits[pos] == 0 && pos != 0) recursion(digits, pos - 1);
-    }
 
     public static void main(String[] args) {
         PlusOne_66 p = new PlusOne_66();
-        p.plusOne(new int[]{99, 99});
+        p.plusOne(new int[]{8,9});
     }
 }
