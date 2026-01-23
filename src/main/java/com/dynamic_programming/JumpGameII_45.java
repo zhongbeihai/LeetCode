3eebdccbd6a8a1342c4f92074bb17dd8c87bd4b8
Logan Wong <huang1002788835@outlook.com>
2026-01-22 21:19:56 -0800

    45

--- src/main/java/com/dynamic_programming/JumpGameII_45.java
+++ src/main/java/com/dynamic_programming/JumpGameII_45.java
@@ -4,17 +4,14 @@ import java.util.Arrays;
 
 public class JumpGameII_45 {
     public int jump(int[] nums) {
-        // dp[i] is minimum number of jumps to i
         int n = nums.length;
-        int[] dp = new int[n];
+        int[] dp = new int[n]; // dp[i] -> minimum step jump to i
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
 
-        for (int i = 1; i < n; i++){
-            for (int j = 0; j < i; j++){
-                if (j + nums[j] >= i){
-                    dp[i] = Math.min(dp[i], dp[j] + 1);
-                }
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < nums[i]; j++) {
+                if (i + j + 1 < n) dp[i + j + 1] = Math.min(dp[i + j + 1], dp[i] + 1);
             }
         }
 
