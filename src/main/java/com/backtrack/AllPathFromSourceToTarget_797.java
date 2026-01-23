98a66eab10e3511d323362b47977fb1e0ad36a45
Logan Wong <huang1002788835@outlook.com>
2026-01-22 23:34:06 -0800

    797

--- src/main/java/com/backtrack/AllPathFromSourceToTarget_797.java
+++ src/main/java/com/backtrack/AllPathFromSourceToTarget_797.java
@@ -1,28 +1,35 @@
 package com.backtrack;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class AllPathFromSourceToTarget_797 {
     private List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        ArrayList<Integer> l = new ArrayList<>();
-        l.add(0);
-        backtrack(graph, l, 0);
+        Map<Integer, List<Integer>> graphs = new HashMap<>();
+        for (int i = 0; i < graph.length; i++) {
+            graphs.putIfAbsent(i, new ArrayList<>());
+            List<Integer> list = graphs.get(i);
+            for (int g: graph[i]) list.add(g);
+        }
+
+        List<Integer> tem = new ArrayList<>();
+        tem.add(0);
+        dfs(graphs, 0, tem);
+
         return res;
     }
 
-    public void backtrack(int[][] graph, List<Integer> tem, int location) {
-        if(location == graph.length - 1) {
+    public void dfs(Map<Integer, List<Integer>> graphs, int idx, List<Integer> tem){
+        if (idx == graphs.size() - 1) {
             res.add(new ArrayList<>(tem));
-        }
-        if(graph[location] == null){
             return;
         }
-        for(int i = 0; i < graph[location].length; i++){
-            tem.add(graph[location][i]);
 
-            backtrack(graph, tem, graph[location][i]);
+        List<Integer> neis = graphs.get(idx);
+        for (int n: neis){
+            tem.add(n);
+
+            dfs(graphs, n, tem);
 
             tem.remove(tem.size() - 1);
         }
