1c09250b425c15480dd919346ed93156284a0f76
Logan Wong <huang1002788835@outlook.com>
2026-01-23 00:00:12 -0800

    49

--- src/main/java/com/hashtable/GroupAnagrams_49.java
+++ src/main/java/com/hashtable/GroupAnagrams_49.java
@@ -8,14 +8,17 @@ import java.util.List;
 public class GroupAnagrams_49 {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
+
         for (String str: strs){
-            char[] c = str.toCharArray();
-            Arrays.sort(c);
-            String anagram = String.valueOf(c);
-            map.computeIfAbsent(anagram, v -> new ArrayList<>()).add(str);
+            char[] strArr = str.toCharArray();
+            Arrays.sort(strArr);
+            map.computeIfAbsent(String.valueOf(strArr), v ->  new ArrayList<String>()).add(str);
         }
 
-        return new ArrayList<>(map.values());
+        List<List<String>> res = new ArrayList<>();
+        res.addAll(map.values());
+
+        return res;
     }
 
     public static void main(String[] args) {
