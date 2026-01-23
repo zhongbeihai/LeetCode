89d9019d1ecf15edb8c409e1d4ed661e391ca7b0
Logan Wong <huang1002788835@outlook.com>
2026-01-22 23:54:42 -0800

    322

--- src/main/java/com/dynamic_programming/CoinChange_322.java
+++ src/main/java/com/dynamic_programming/CoinChange_322.java
@@ -10,13 +10,13 @@ import java.util.Arrays;
  */
 class CoinChange_322 {
     public int coinChange(int[] coins, int amount){
-        int[] dp = new int[amount + 1];
+        int[] dp = new int[amount + 1]; // dp[i] -> the number of coin to get i
         Arrays.fill(dp, Integer.MAX_VALUE);
+
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int coin: coins){
-                if (i - coin < 0 || dp[i - coin] == Integer.MAX_VALUE) continue;
-                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
 
