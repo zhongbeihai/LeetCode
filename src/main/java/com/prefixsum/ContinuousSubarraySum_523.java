b496a4791d46db6b6922931f911c4a0c48aab4f5
Logan Wong <huang1002788835@outlook.com>
2026-01-25 17:50:29 -0800

    523

--- src/main/java/com/prefixsum/ContinuousSubarraySum_523.java
+++ src/main/java/com/prefixsum/ContinuousSubarraySum_523.java
@@ -5,39 +5,27 @@ import java.util.Map;
 
 public class ContinuousSubarraySum_523 {
     public boolean checkSubarraySum(int[] nums, int k) {
-        // sum(i, j) % k == 0;
-        // (prefixSum[j] - prefixSum[i]) % k == 0;
-        // prefixSum[j] % k == prefixSum[i] % k
+        Map<Integer, Integer> restLastOccurIdx = new HashMap<>();
+        restLastOccurIdx.put(0, -1);
 
-        int n = nums.length;
-        if (k == 0) {
-            for (int i = 1; i < n; i++) {
-                if (nums[i] == 0 && nums[i - 1] == 0) return true;
-            }
-            return false;
-        }
+        int curSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curSum += nums[i];
 
-
-        int[] p = new int[n];
-        p[0] = nums[0];
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        map.putIfAbsent(p[0] % k, 0);
-        for (int i = 1; i < n; i++) {
-            p[i] = p[i - 1] + nums[i];
-            if (map.containsKey(p[i] % k) ){
-                if (i - map.get(p[i] % k) >= 2) return true;
+            int r = curSum % k;
+            if (r < 0) r += k;
+            if (restLastOccurIdx.containsKey(r)){
+                if (i - restLastOccurIdx.get(r) > 1 )return true;
             }else {
-                map.put(p[i] % k, i);
+                restLastOccurIdx.put(r, i);
             }
         }
-
 
         return false;
     }
 
     public static void main(String[] args) {
         ContinuousSubarraySum_523 c = new ContinuousSubarraySum_523();
-        c.checkSubarraySum(new int[]{23,2,4,6,6}, 7);
+        c.checkSubarraySum(new int[]{2, 4, 3}, 6);
     }
 }
