094895507d42f2fce8f82326f7e6feda23e4326c
Logan Wong <huang1002788835@outlook.com>
2026-01-26 23:53:03 -0800

    1209

--- src/main/java/com/stack/RemoveAllAdjacentDuplicatesInStringII_1209.java
+++ src/main/java/com/stack/RemoveAllAdjacentDuplicatesInStringII_1209.java
@@ -2,26 +2,31 @@ package com.stack;
 
 import structure.Pair;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class RemoveAllAdjacentDuplicatesInStringII_1209 {
     public String removeDuplicates(String s, int k) {
-        Stack<int[]> stack = new Stack<>();
+        Deque<int[]> stack = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && s.charAt(i) == s.charAt(stack.peek()[0]) && stack.peek()[1] == k - 1){
-                for (int j = 0; j < k - 1; j++) {
-                    stack.pop();
-                }
+            if (stack.isEmpty() || s.charAt(i) != s.charAt(stack.peek()[0])){
+                stack.push(new int[]{i, 1});
             }else {
-                if (stack.isEmpty() || s.charAt(stack.peek()[0]) != s.charAt(i)) stack.push(new int[]{i, 1});
-                else stack.push(new int[]{i, stack.peek()[1] + 1});
+                if (stack.peek()[1] == k - 1)stack.pop();
+                else {
+                    stack.peek()[1] += 1;
+                }
             }
         }
 
         StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()){
-            sb.append(s.charAt(stack.pop()[0]));
+            int[] top = stack.pop();
+            for (int i = 0; i < top[1]; i++) {
+                sb.append(s.charAt(top[0]));
+            }
         }
 
         return sb.reverse().toString();
