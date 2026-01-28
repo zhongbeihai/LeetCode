131d8f117948ec70dc477201b88495489122c09d
Logan Wong <huang1002788835@outlook.com>
2026-01-27 21:10:24 -0800

    79

--- src/main/java/com/search/WordSearch_79.java
+++ src/main/java/com/search/WordSearch_79.java
@@ -7,31 +7,37 @@ import java.util.Queue;
 
 public class WordSearch_79 {
     private int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
     public boolean exist(char[][] board, String word) {
-        int n = board.length, m = board[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (board[i][j] == word.charAt(0) && dfs(board, word, 0, new boolean[n][m], i, j)) return true;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word.charAt(0) == board[i][j]) {
+                    boolean flag = dfs(board, word, new boolean[m][n], 0, i, j);
+                    if (flag) return true;
+                }
             }
         }
 
         return false;
     }
 
-    public boolean dfs(char[][] board, String word, int idx, boolean[][] visited, int x, int y){
-        int n = board.length, m = board[0].length;
-
+    public boolean dfs(char[][] board, String word, boolean[][] visited, int idx, int x, int y){
         if (idx == word.length() - 1) return true;
+
         visited[x][y] = true;
+        boolean flag = false;
         for (int[] dir: dirs){
             int nx = x + dir[0], ny = y + dir[1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == word.charAt(idx + 1) && !visited[nx][ny]){
-                if( dfs(board, word, idx + 1, visited, nx, ny)) return true;
+            if (nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length
+                    && !visited[nx][ny] && word.charAt(idx + 1) == board[nx][ny]){
+                flag |= dfs(board, word, visited, idx + 1, nx , ny);
             }
         }
-
         visited[x][y] = false;
-        return false;
+
+        return flag;
     }
 
 
