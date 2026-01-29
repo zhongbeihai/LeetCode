3d108d29c6be25cfb3ac4752f4620e0ae96640b2
Logan Wong <huang1002788835@outlook.com>
2026-01-28 23:12:18 -0800

    72

--- src/main/java/com/dynamic_programming/EditDistance_72.java
+++ src/main/java/com/dynamic_programming/EditDistance_72.java
@@ -2,21 +2,29 @@ package com.dynamic_programming;
 
 public class EditDistance_72 {
     public int minDistance(String word1, String word2) {
-        int n = word1.length(), m = word2.length();
-        int[][] dp = new int[n+ 1][m + 1];
-        for (int i = 0; i < m; i++) dp[i][0] = i;
-        for (int i = 0; i < n; i++) dp[0][i] = i;
+        if (word1.isEmpty()) return word2.length();
+        if (word2.isEmpty()) return word1.length();
 
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
 
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= m; j++){
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <=n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = 1 + Math.min(dp[i - 1][j - 1],  // replace
-                        Math.min(dp[i][j - 1], // delete
-                                dp[i - 1][j])); // insert
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], // replace
+                            Math.min(dp[i - 1][j],  // insert
+                                    dp[i][j - 1])); // delete
+                }
             }
         }
 
-        return dp[n][m];
+        return dp[m][n];
     }
 }
