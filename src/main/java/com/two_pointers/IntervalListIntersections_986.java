ac9849729cf856a97c4418b33c54a18aedf2a033
Logan Wong <huang1002788835@outlook.com>
2026-02-07 13:46:56 -0800

    986

--- src/main/java/com/two_pointers/IntervalListIntersections_986.java
+++ src/main/java/com/two_pointers/IntervalListIntersections_986.java
@@ -5,22 +5,23 @@ import java.util.List;
 
 public class IntervalListIntersections_986 {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        if (firstList.length == 0 || secondList.length == 0) return new int[][]{};
+        if (firstList.length ==0 || secondList.length == 0) return new int[0][0];
 
-        int fp = 0, sp = 0;
         List<int[]> res = new ArrayList<>();
-
+        int fp = 0, sp = 0;
         while (fp < firstList.length && sp < secondList.length){
             int[] f = firstList[fp], s = secondList[sp];
-            int start = Math.max(f[0], s[0]), end = Math.min(f[1], s[1]);
-            if (start <= end) res.add(new int[]{start, end});
-            if (f[1] < s[1]) fp++;
-            else sp++;
+            if (f[1] >= s[0] && f[0] <= s[1]) res.add(new int[]{Math.max(f[0], s[0]), Math.min(f[1], s[1])});
 
+            if (f[1] <= s[1]) fp++;
+            else sp++;
         }
 
-        int[][] r = new int[res.size()][];
-        for (int i = 0; i < res.size(); i++) r[i] = res.get(i);
+        int[][] r = new int[res.size()][2];
+        for (int i = 0; i < res.size(); i++) {
+            r[i] = res.get(i);
+        }
+
         return r;
     }
 
