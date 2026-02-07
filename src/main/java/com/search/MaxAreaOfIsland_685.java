ae2fe6b1b62c1dfd1e73dec6fde62978b7b8f43e
Logan Wong <huang1002788835@outlook.com>
2026-02-07 14:06:35 -0800

    685

--- src/main/java/com/search/MaxAreaOfIsland_685.java
+++ src/main/java/com/search/MaxAreaOfIsland_685.java
@@ -3,32 +3,32 @@ package com.search;
 public class MaxAreaOfIsland_685 {
     private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
         int maxArea = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 0) continue;
-                if(grid[i][j] == 1){
-                    maxArea = Math.max(maxArea, dfs(grid, i, j));
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) maxArea = Math.max(maxArea, dfs(grid, i, j));
             }
         }
+
         return maxArea;
     }
 
-    public int dfs(int[][] grid, int i, int j){
-
-        int area = 1;
-        grid[i][j] = 2;
-        for(int[] direction : directions){
-            int row = i + direction[0];
-            int col = j + direction[1];
-            if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length
-                    && grid[row][col] == 1){
-                area += dfs(grid, row, col);
+    public int dfs(int[][] grid, int x, int y){
+        grid[x][y] = 0;
+        int r = 1;
+        for (int[] dir: directions){
+            int nx = x + dir[0], ny = y + dir[1];
+            if (nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 1){
+                r += dfs(grid, nx, ny);
             }
         }
-        return area;
+
+        return r;
     }
+
+
 
     public static void main(String[] args) {
         MaxAreaOfIsland_685 m =  new MaxAreaOfIsland_685();
