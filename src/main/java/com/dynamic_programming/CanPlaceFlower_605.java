62d26ecc6d502051d6fa6471b9acaeee795d124e
Logan Wong <huang1002788835@outlook.com>
2026-02-10 18:25:43 -0800

    605

--- src/main/java/com/dynamic_programming/CanPlaceFlower_605.java
+++ src/main/java/com/dynamic_programming/CanPlaceFlower_605.java
@@ -2,17 +2,16 @@ package com.dynamic_programming;
 
 public class CanPlaceFlower_605 {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0) return true;
-        int m = flowerbed.length;
-        for (int i = 0; i < m; i++){
-            if (flowerbed[i] != 0) continue;
-            if (i - 1 >= 0 && flowerbed[i - 1] == 1) continue;
-            if (i + 1 < m && flowerbed[i + 1] == 1) continue;
+        if (n == 0 ) return true;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 1) continue;
+            if (i != flowerbed.length - 1 && flowerbed[i + 1] == 1) continue;
+            if (i != 0 && flowerbed[i - 1] == 1) continue;
 
             n--;
             flowerbed[i] = 1;
-
-            if (n == 0) return true;
+            if(n == 0) return true;
         }
 
         return false;
