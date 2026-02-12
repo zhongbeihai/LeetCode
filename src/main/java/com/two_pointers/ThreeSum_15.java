a4d0bf588d24db31151344d6d935fe6d4bd70f3f
Logan Wong <huang1002788835@outlook.com>
2026-02-12 00:09:27 -0800

    15

--- src/main/java/com/two_pointers/ThreeSum_15.java
+++ src/main/java/com/two_pointers/ThreeSum_15.java
@@ -10,7 +10,7 @@ public class ThreeSum_15 {
 
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length - 2; i++){
             if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1, right = nums.length - 1;
@@ -19,6 +19,8 @@ public class ThreeSum_15 {
                 if (sum == 0){
                     List<Integer> tem = Arrays.asList(nums[i], nums[left], nums[right]);
                     res.add(new ArrayList<>(tem));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 } else if (sum > 0) {
