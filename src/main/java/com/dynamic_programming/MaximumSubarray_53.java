4be60aae3a2cf5f4e6723b8249f60fa7cff488d0
Logan Wong <huang1002788835@outlook.com>
2026-02-12 20:24:31 -0800

    53

--- src/main/java/com/dynamic_programming/MaximumSubarray_53.java
+++ src/main/java/com/dynamic_programming/MaximumSubarray_53.java
@@ -4,15 +4,13 @@ public class MaximumSubarray_53 {
     public int maxSubArray(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int dpMax = nums[0]; // maintain the maximum number ending at i
-        int ans = nums[0];
-
+        int[] dp = new int[nums.length];
+        int maxSum = nums[0];
+        dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++){
-            dpMax = Math.max(nums[i], dpMax + nums[i]);
-
-            ans = Math.max(ans, dpMax);
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, dp[i]);
         }
-
-        return ans;
+        return maxSum;
     }
 }
