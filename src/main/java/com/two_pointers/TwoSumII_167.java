547b4b6728eab02db02e81adabdc0759565e6593
Logan Wong <huang1002788835@outlook.com>
2026-02-12 21:37:48 -0800

    167

--- src/main/java/com/two_pointers/TwoSumII_167.java
+++ src/main/java/com/two_pointers/TwoSumII_167.java
@@ -4,20 +4,17 @@ public class TwoSumII_167 {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
 
-        int[] res = new int[2];
         while (left < right){
-            if (numbers[left] + numbers[right] == target){
-                res[0] = left + 1;
-                res[1] = right + 1;
-                return res;
+            int sum = numbers[left] + numbers[right];
+            if (sum == target){
+                return new int[]{left + 1, right + 1};
             }
-            if (numbers[left] + numbers[right] > target){
+            if (sum > target){
                 right--;
             }else {
                 left++;
             }
         }
-
-        return res;
+        return new int[]{};
     }
 }
