9d458ce98ca5e90dbbb08e18c838b0cefd7bdbb6
Logan Wong <huang1002788835@outlook.com>
2026-02-12 20:27:14 -0800

    1768

--- src/main/java/com/two_pointers/MergeStringAlternately_1768.java
+++ src/main/java/com/two_pointers/MergeStringAlternately_1768.java
@@ -2,23 +2,20 @@ package com.two_pointers;
 
 public class MergeStringAlternately_1768 {
     public String mergeAlternately(String word1, String word2) {
-        // p1 -> word1, p2 -> word2
-        int p1 = 0, p2 = 0;
-        char[] w1 = word1.toCharArray();
-        char[] w2 = word2.toCharArray();
         int m = word1.length(), n = word2.length();
+        int p1 = 0, p2 = 0;
+
         StringBuilder sb = new StringBuilder();
         while (p1 < m && p2 < n){
-            sb.append(w1[p1]);
-            sb.append(w2[p2]);
+            sb.append(word1.charAt(p1)).append(word2.charAt(p2));
             p1++;
             p2++;
         }
-
-        if (m > n){
-            while (p1 < m) sb.append(w1[p1++]);
-        }else {
-            while (p2 < n) sb.append(w2[p2++]);
+        while (p1 < m){
+            sb.append(word1.charAt(p1++));
+        }
+        while (p2 < n){
+            sb.append(word2.charAt(p2++));
         }
 
         return sb.toString();
