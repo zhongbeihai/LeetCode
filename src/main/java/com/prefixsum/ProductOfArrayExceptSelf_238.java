faf2cefa5d29d1c19c99f887cfc51554f2b3cd1d
Logan Wong <huang1002788835@outlook.com>
2026-02-13 22:45:13 -0800

    238

--- src/main/java/com/prefixsum/ProductOfArrayExceptSelf_238.java
+++ src/main/java/com/prefixsum/ProductOfArrayExceptSelf_238.java
@@ -2,22 +2,22 @@ package com.prefixsum;
 
 public class ProductOfArrayExceptSelf_238 {
     public int[] productExceptSelf(int[] nums) {
-        int zeroCnt = 0;
-        int prod = 1;
-        for (int num: nums){
-            if (num != 0) prod *= num;
-            else zeroCnt++;
+        int n = nums.length;
+        int[] prefix = new int[n + 1], suffix = new int[n + 1];
+        prefix[0] = 1;
+        suffix[n] = 1;
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 1; i >= 0 ; i--) {
+            suffix[i] = suffix[i + 1] * nums[i];
         }
 
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0){
-                if (zeroCnt == 1) res[i] = prod;
-                else res[i] = 0;
-            }else {
-                if (zeroCnt == 0) res[i] = prod / nums[i];
-                else res[i] = 0;
-            }
+        prefix[0] = 1;
+        suffix[n] = 1;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++){
+            res[i] = prefix[i] * suffix[i + 1];
         }
 
         return res;
