bfbe7bf37be4781a18e7a55827703704db45d62f
Logan Wong <huang1002788835@outlook.com>
2026-02-15 20:44:13 -0800

    56

--- src/main/java/com/simulation/MergeIntervals_56.java
+++ src/main/java/com/simulation/MergeIntervals_56.java
@@ -7,21 +7,29 @@ import java.util.stream.Collectors;
 
 public class MergeIntervals_56 {
     public int[][] merge(int[][] intervals) {
-       Arrays.sort(intervals, Comparator.comparing(i -> i[0]));
+        if (intervals.length < 2) return intervals;
 
-       List<int[]> list = new ArrayList<>();
-       list.add(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            int[] last = list.get(list.size() - 1), cur = intervals[i];
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        int n = intervals.length;
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
 
-            if (cur[0] > last[1]) list.add(cur);
-            else {
-                last[1] = Math.max(last[1], cur[1]);
+        for (int i = 1; i < n; i++) {
+            int[] last = list.get(list.size() - 1);
+            int[] cur = intervals[i];
+            if (cur[0] > last[1]) {
+                list.add(intervals[i]);
+            }else {
+                list.remove(list.size() - 1);
+                int start = Math.min(last[0], cur[0]);
+                int end = Math.max(last[1], cur[1]);
+
+                list.add(new int[]{start, end});
             }
         }
 
-        int[][] res = new int[list.size()][2];
-        for (int i = 0; i < res.length; i++) {
+        int[][] res = new int[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
         }
 
