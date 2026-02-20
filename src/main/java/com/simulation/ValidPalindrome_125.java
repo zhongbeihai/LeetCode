94405739a7a7ea3e9b10c42f5da0395cacb11f27
Logan Wong <huang1002788835@outlook.com>
2026-02-19 22:56:45 -0800

    125

--- src/main/java/com/simulation/ValidPalindrome_125.java
+++ src/main/java/com/simulation/ValidPalindrome_125.java
@@ -2,30 +2,21 @@ package com.simulation;
 
 public class ValidPalindrome_125 {
     public boolean isPalindrome(String s) {
-        int n = s.length();
-
-        StringBuilder sb = new StringBuilder();
-        for(char c: s.toCharArray()){
-            if (Character.isLetter(c)) sb.append(Character.toLowerCase(c));
-            if (Character.isDigit(c)) sb.append(c);
-        }
-
-        char[] charArr = sb.toString().toCharArray();
-        int left = 0, right = charArr.length - 1;
+        char[] ss = s.toCharArray();
+        int left = 0, right = ss.length - 1;
         while (left < right){
-            if (charArr[left] != charArr[right]) {
-                return false;
-            }
+            while(left < right && (!Character.isLetter(ss[left]) && !Character.isDigit(ss[left]))) left++;
+            while (left < right && (!Character.isLetter(ss[right]) && !Character.isDigit(ss[right]))) right--;
+            if (Character.toLowerCase(ss[left]) != Character.toLowerCase(ss[right])) return false;
             left++;
             right--;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         ValidPalindrome_125 v = new ValidPalindrome_125();
-        v.isPalindrome("A man, a plan, a canal: Panama");
+        v.isPalindrome("race a car");
         // abcca
     }
 }
