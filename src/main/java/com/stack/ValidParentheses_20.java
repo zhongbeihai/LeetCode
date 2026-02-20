0ef03b56d9fa6fe814d4f79899a7e6a86d005e1a
Logan Wong <huang1002788835@outlook.com>
2026-02-20 13:36:13 -0800

    20

--- src/main/java/com/stack/ValidParentheses_20.java
+++ src/main/java/com/stack/ValidParentheses_20.java
@@ -1,19 +1,19 @@
 package com.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
 // determine if the input string is valid.
 public class ValidParentheses_20 {
     public boolean isValid(String s) {
-       char[] ss = s.toCharArray();
-       Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < ss.length; i++) {
-            if (stack.isEmpty()) stack.add(i);
-            else if (ss[i] == '[' || ss[i] == '{' || ss[i] == '(') stack.add(i);
-            else if (ss[i] == ']' && ss[stack.peek()] == '[') stack.pop();
-            else if (ss[i] == '}' && ss[stack.peek()] == '{') stack.pop();
-            else if (ss[i] == ')' && ss[stack.peek()] == '(') stack.pop();
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c: s.toCharArray()){
+            if (stack.isEmpty() || c == '(' || c == '[' || c == '{') stack.push(c);
+            else if (c == ')' && stack.peek() == '(') stack.pop();
+            else if (c == '}' && stack.peek() == '{') stack.pop();
+            else if (c == ']' && stack.peek() == '[') stack.pop();
             else return false;
         }
 
