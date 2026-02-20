684be5dc187fd4433baa5428451359a45183bd3e
Logan Wong <huang1002788835@outlook.com>
2026-02-19 22:17:16 -0800

    383

--- src/main/java/com/hashtable/ContainsDuplicate_217.java
+++ src/main/java/com/hashtable/ContainsDuplicate_217.java
@@ -6,10 +6,8 @@ public class ContainsDuplicate_217 {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int num: nums){
-            if (set.contains(num)) return false;
-            set.add(num);
+            if (!set.add(num)) return false;
         }
-
         return true;
     }
 }
