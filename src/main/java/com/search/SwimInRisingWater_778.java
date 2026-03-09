02b5a1b66e7262d8b826b127e7ac394919580d0d
Logan Wong <huang1002788835@outlook.com>
2026-03-08 23:04:47 -0700

    778

--- src/main/java/com/search/SwimInRisingWater_778.java
+++ src/main/java/com/search/SwimInRisingWater_778.java
@@ -1,13 +1,48 @@
 package com.search;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 public class SwimInRisingWater_778 {
     private int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        int lo = grid[0][0], hi = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                hi = Math.max(hi, grid[i][j]);
+            }
+        }
+
+        while (lo < hi){
+            int mid = lo + (hi - lo) / 2;
+            if (dfs(grid, 0, 0, mid, new boolean[n][n])){
+                hi = mid;
+            }else {
+                lo = mid + 1;
+            }
+        }
+
+        return hi;
+    }
+
+    public boolean dfs(int[][]grid, int x, int y, int curHeight, boolean[][] visited){
+        int n = grid.length;
+        visited[x][y] = true;
+
+        if (x == n - 1 && y == n - 1) return true;
+
+        boolean flag = false;
+        for (int[] dir: dirs){
+            int nx = x + dir[0], ny = y + dir[1];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && curHeight >= grid[nx][ny])
+                flag |=  dfs(grid, nx, ny, curHeight, visited);
+        }
+
+        return flag;
+    }
+
+    public int Dijkstra(int[][] grid){
         int n = grid.length;
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(i -> i[2])); // {x, y, time}
         pq.add(new int[]{0, 0, grid[0][0]});
