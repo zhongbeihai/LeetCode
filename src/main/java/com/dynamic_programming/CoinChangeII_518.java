39bb2ed2ae9dfabb355c8c0616c6d93e3f9ed741
Logan Wong <huang1002788835@outlook.com>
2026-03-10 23:01:06 -0700

    518

--- src/main/java/com/dynamic_programming/CoinChangeII_518.java
+++ src/main/java/com/dynamic_programming/CoinChangeII_518.java
@@ -2,12 +2,12 @@ package com.dynamic_programming;
 
 public class CoinChangeII_518 {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-
-        dp[0] = 1;
+        int[] dp = new int[amount + 1]; // dp[i] -> the number of ways to get amount i
+         dp[0] = 1;
+        // dp[i] += dp[i - coin]
         for (int coin: coins){
-            for (int i = coin; i <= amount; i++){
-                dp[i] += dp[i - coin];
+            for (int i = 0; i <= amount; i++){
+                if (i - coin >= 0) dp[i] += dp[i - coin];
             }
         }
 
