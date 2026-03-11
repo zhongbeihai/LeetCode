4eb6ed581edba1115306490dceba922cd274c923
Logan Wong <huang1002788835@outlook.com>
2026-03-10 23:12:35 -0700

    249

--- src/main/java/com/search/GroupShiftedStirngs_249.java
+++ src/main/java/com/search/GroupShiftedStirngs_249.java
@@ -7,24 +7,25 @@ import java.util.Map;
 
 public class GroupShiftedStirngs_249 {
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> stringToKey = new HashMap<>();
-        for (String s: strings) {
-            String k= getKeyOfString(s);
-            stringToKey.computeIfAbsent(k, i -> new ArrayList<>()).add(s);
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strings){
+            String key = getKeyOfString(s);
+            map.computeIfAbsent(key, i -> new ArrayList<>()).add(s);
         }
-
-        return new ArrayList<>(stringToKey.values());
-
+        return new ArrayList<>(map.values());
     }
 
     public String getKeyOfString(String s){
-        if (s.length() == 1) return "";
+        if (s.length() == 1) return "1#1";
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < s.length(); i++) {
-            int diff = (s.charAt(i) - s.charAt(i - 1) + 26) % 26;
-            sb.append(diff).append('#');
+            int gap = s.charAt(i) - s.charAt(i - 1);
+            if (gap < 0) gap += 26;
+            gap %= 26;
+            sb.append(gap).append("#");
         }
+
         return sb.toString();
     }
 }
