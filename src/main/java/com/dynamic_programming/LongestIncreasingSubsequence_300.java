fc38fb03b2a1fdd60a8325c6d5c17a6a8d1b7b05
Logan Wong <huang1002788835@outlook.com>
2026-03-12 00:13:17 -0700

    300

--- src/main/java/com/dynamic_programming/LongestIncreasingSubsequence_300.java
+++ src/main/java/com/dynamic_programming/LongestIncreasingSubsequence_300.java
@@ -11,13 +11,15 @@ public class LongestIncreasingSubsequence_300 {
         Arrays.fill(dp, 1);
 
         // dp[i] = dp[j] + 1; and j < i and nums[j] < nums[i]
+        int res = 0;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                res = Math.max(res, dp[i]);
             }
         }
 
-        return dp[n - 1];
+        return res;
     }
 
     public int lengthOfLISImprove(int[] nums){
