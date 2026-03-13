1dfa3a544926c98b1a3bd6da242967165f98ebde
Logan Wong <huang1002788835@outlook.com>
2026-03-12 23:25:55 -0700

    2007

--- src/main/java/com/simulation/FindOriginalArrayFromDoubledArray_2007.java
+++ src/main/java/com/simulation/FindOriginalArrayFromDoubledArray_2007.java
@@ -4,42 +4,28 @@ import java.util.*;
 
 public class FindOriginalArrayFromDoubledArray_2007 {
     public int[] findOriginalArray(int[] changed) {
-        int n = changed.length;
-        if (n % 2 == 1) return new int[0];
-
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int num : changed) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> fre = new HashMap<>();
+        for (int c : changed){
+            fre.put(c, fre.getOrDefault(c, 0) + 1);
         }
 
-        List<Integer> keys = new ArrayList<>(map.keySet());
-        int[] res = new int[n / 2];
-        int j = 0;
+        int[] res = new int[changed.length / 2];
+        int i = 0;
+        for (int num: fre.keySet()){
+            if (fre.get(num) == 0) continue;
 
-        if (map.containsKey(0)) {
-            if (map.get(0) % 2 == 1) return new int[0];
-            for (int fre = 0; fre < map.get(0) / 2; fre++) {
-                res[j++] = 0;
+            if (num == 0){
+                if (fre.get(num) < 2) return new int[0];
+                fre.put(num, fre.getOrDefault(num, 0) - 2);
+            }else {
+                if (fre.getOrDefault(num * 2, 0) < fre.get(num)) return new int[0];
+                fre.put(num, fre.get(num) - 1);
+                fre.put(num * 2, fre.get(num * 2) - 1);
             }
-            map.put(0, 0);
+            res[i++] = num;
         }
 
-        for (int i = 0; i < keys.size(); i++) {
-            int cur = keys.get(i);
-            if (cur == 0 || map.get(cur) == 0) continue;
-
-            int cnt = map.get(cur);
-            if (!map.containsKey(cur * 2) || map.get(cur * 2) < cnt) return new int[0];
-
-            for (int fre = 0; fre < cnt; fre++) {
-                res[j++] = cur;
-            }
-
-            map.put(cur * 2, map.get(cur * 2) - cnt);
-            map.put(cur, 0);
-        }
-
-        return j == n / 2 ? res : new int[0];
+        return res;
     }
 
     public static void main(String[] args) {
