dd7e0672807cde40b49eb040a0af0aab995705fe
Logan Wong <huang1002788835@outlook.com>
2026-03-15 17:57:48 -0700

    2007

--- src/main/java/com/simulation/FindOriginalArrayFromDoubledArray_2007.java
+++ src/main/java/com/simulation/FindOriginalArrayFromDoubledArray_2007.java
@@ -4,27 +4,31 @@ import java.util.*;
 
 public class FindOriginalArrayFromDoubledArray_2007 {
     public int[] findOriginalArray(int[] changed) {
-        if (changed.length % 2 == 1) return new int[0];
-        Arrays.sort(changed);
         Map<Integer, Integer> fre = new HashMap<>();
-        for (int c : changed) {
-            fre.put(c, fre.getOrDefault(c, 0) + 1);
-        }
+        for (int c: changed) fre.put(c, fre.getOrDefault(c, 0) + 1);
 
         int[] res = new int[changed.length / 2];
-        int i = 0;
+        int j = 0;
+        Arrays.sort(changed);
+        if (fre.containsKey(0)){
+            if (fre.get(0) % 2 == 1) return new int[0];
+        }
+        for (int c: changed){
+            int curFre = fre.get(c);
+            if (curFre == 0) continue;
+            if (!fre.containsKey(c * 2) || fre.get(c * 2) < curFre) return new int[0];
 
-        for (int num : changed) {
-            if (fre.get(num) == 0) continue;
+            res[j++] = c;
+            if (c == 0){
+                fre.put(c, fre.get(c) - 2);
+            }else {
+                fre.put(c, fre.get(c) - 1);
+                fre.put(c * 2, fre.get(c * 2) - 1);
+            }
 
-            if (fre.getOrDefault(num * 2, 0) == 0) return new int[0];
-
-            res[i++] = num;
-            fre.put(num, fre.get(num) - 1);
-            fre.put(num * 2, fre.get(num * 2) - 1);
         }
 
-        return i == changed.length / 2 ? res : new int[0];
+        return j == changed.length / 2 ? res : new int[0];
     }
 
     public static void main(String[] args) {
