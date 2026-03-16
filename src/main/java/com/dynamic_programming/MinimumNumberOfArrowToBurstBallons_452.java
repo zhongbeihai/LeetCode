b2be5dc3eaf3b10a1df951222d4e35b83cf1ff7c
Logan Wong <huang1002788835@outlook.com>
2026-03-16 01:23:24 -0700

    452

--- src/main/java/com/dynamic_programming/MinimumNumberOfArrowToBurstBallons_452.java
+++ src/main/java/com/dynamic_programming/MinimumNumberOfArrowToBurstBallons_452.java
@@ -1,26 +1,33 @@
 package com.dynamic_programming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class MinimumNumberOfArrowToBurstBallons_452 {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, ((a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1])));
+        Arrays.sort(points, Comparator.comparingInt(i -> i[0]));
 
-        int count = 1, l = points[0][0], r = points[0][1];
-        for (int i = 1; i < points.length; i++) {
-            if (points[i][0] > r ) {
-                count++;
-                r = points[i][1];
+        List<int[]> res = new ArrayList<>();
+        res.add(points[0]);
+        for(int i = 1; i < points.length; i++){
+            int[] last = res.get(res.size() - 1), cur = points[i];
+
+            if (cur[0] <= last[1]){
+                last[0] = Math.max(last[0], cur[0]);
+                last[1] = Math.min(last[1], cur[1]);
+            }else {
+                res.add(cur);
             }
-            r = Math.min(r, points[i][1]);
         }
 
-        return count;
+        return res.size();
     }
 
     public static void main(String[] args) {
         MinimumNumberOfArrowToBurstBallons_452 m = new MinimumNumberOfArrowToBurstBallons_452();
-        //m.findMinArrowShots(new int[][]{{10,16},{2,8},{1,6},{7,12}});
+        m.findMinArrowShots(new int[][]{{10,16},{2,8},{1,6},{7,12}});
         m.findMinArrowShots(new int[][]{{9,12},{1,10},{4,11},{8,12},{3,9},{6,9},{6,7}});
     }
 }
