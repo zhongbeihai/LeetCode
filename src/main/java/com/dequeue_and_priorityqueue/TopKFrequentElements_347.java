1180059933b1f55ee028e10f40b58d094deecf5d
Logan Wong <huang1002788835@outlook.com>
2026-03-16 13:52:07 -0700

    347

--- src/main/java/com/dequeue_and_priorityqueue/TopKFrequentElements_347.java
+++ src/main/java/com/dequeue_and_priorityqueue/TopKFrequentElements_347.java
@@ -7,33 +7,19 @@ import java.util.*;
  * return the k most frequent elements. You may return the answer in any order.
  */
 public class TopKFrequentElements_347 {
-
-    // use priority queue
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> fre = new HashMap<>();
-        for(int num: nums){
-            fre.put(num, fre.getOrDefault(num, 0) + 1);
+        HashMap<Integer, Integer> fre = new HashMap<>();
+        for (int num: nums){
+            fre.put(num, fre.getOrDefault(num , 0) + 1);
         }
 
-        List<Integer>[] buckets = new List[100000];
-        for(Map.Entry<Integer, Integer> e: fre.entrySet()){
-            if (buckets[e.getValue()] == null) buckets[e.getValue()] = new ArrayList<>();
-            buckets[e.getValue()].add(e.getKey());
-        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(i -> -i.getValue()));
+        pq.addAll(fre.entrySet());
 
         int[] res = new int[k];
-        int j = 0;
-        for (int i = buckets.length - 1; i >= 0; i--){
-            if (j >= k) break;
-            if (buckets[i] != null){
-                List<Integer> list = buckets[i];
-                int p = list.size() - 1;
-                while (j < k && p >= 0) {
-                    res[j++] = list.get(p--);
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll().getKey();
         }
-
         return res;
     }
 
