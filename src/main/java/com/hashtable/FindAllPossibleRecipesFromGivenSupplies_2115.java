f3040574821b5a9eaac0c58120773e065f21426e
Logan Wong <huang1002788835@outlook.com>
2026-03-16 01:10:30 -0700

    2115

--- src/main/java/com/hashtable/FindAllPossibleRecipesFromGivenSupplies_2115.java
+++ src/main/java/com/hashtable/FindAllPossibleRecipesFromGivenSupplies_2115.java
@@ -4,41 +4,42 @@ import java.util.*;
 
 public class FindAllPossibleRecipesFromGivenSupplies_2115 {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        // < ingredients, List<recipes>>
-        HashMap<String, List<String>> unlockMap = new HashMap<>();
-        // <recipes, number of ingredients needed>
-        HashMap<String, Integer> needCount = new HashMap<>();
+        Map<String, Set<String>> recToIng = new HashMap<>();
+        for (int i = 0; i < recipes.length; i++) recToIng.put(recipes[i], new HashSet<>(ingredients.get(i)));
+        HashSet<String> suppliesSet = new HashSet<>(Arrays.asList(supplies));
 
-        for (int i = 0; i < recipes.length; i++){
-            needCount.put(recipes[i], ingredients.get(i).size());
-            for (String ingredient: ingredients.get(i)){
-                unlockMap.putIfAbsent(ingredient, new ArrayList<>());
-                unlockMap.get(ingredient).add(recipes[i]);
-            }
+        List<String> result = new ArrayList<>();
+        HashMap<String, Integer> status = new HashMap<>(); // 1 -> can't make; 2 -> searching; 3-> can make it
+
+        for (String rep: recipes){
+            if (dfs(recToIng, suppliesSet, rep, status)) result.add(rep);
         }
 
-        List<String> res = new ArrayList<>();
-        HashSet<String> visited = new HashSet<>(Arrays.asList(supplies));
-        Queue<String> queue = new ArrayDeque<>(Arrays.asList(supplies));
-
-        while (!queue.isEmpty()){
-            String ing = queue.poll();
-            visited.add(ing);
-
-            List<String> recis = unlockMap.get(ing);
-            if (recis == null) continue;
-            for (String re: recis){
-                needCount.put(re, needCount.get(re) - 1);
-                if (needCount.get(re) == 0){
-                    needCount.remove(re);
-                    res.add(re);
-                    if (!visited.contains(re)) {
-                        visited.add(re);
-                        queue.add(re);
-                    }
-                }
-            }
-        }
-        return res;
+        return result;
     }
+
+    public boolean dfs(Map<String, Set<String>> recToIng, HashSet<String> suppliesSet,
+                       String recipe, HashMap<String, Integer> status){
+        // return condition
+        if (suppliesSet.contains(recipe)) return true;
+        if (!recToIng.containsKey(recipe)) return false;
+        int s = status.getOrDefault(recipe, 0);
+        if (s == 1 || s == 2) return false;
+        if (s == 3) return true;
+
+        status.put(recipe, 2);
+
+        for (String ing: recToIng.get(recipe)){
+            if (!dfs(recToIng, suppliesSet, ing, status)){
+                status.put(ing, 1);
+                return false;
+            }
+        }
+
+        status.put(recipe, 3);
+        suppliesSet.add(recipe);
+        return true;
+    }
+
+
 }
