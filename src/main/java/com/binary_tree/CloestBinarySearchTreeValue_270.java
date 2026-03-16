ecdbf94cf07cf3cbb39d4e37f4cb96a79fc3ff30
Logan Wong <huang1002788835@outlook.com>
2026-03-16 15:29:34 -0700

    270

--- src/main/java/com/binary_tree/CloestBinarySearchTreeValue_270.java
+++ src/main/java/com/binary_tree/CloestBinarySearchTreeValue_270.java
@@ -3,25 +3,22 @@ package com.binary_tree;
 import structure.TreeNode;
 
 public class CloestBinarySearchTreeValue_270 {
-    private double diff;
-    private int val;
+    private double diff = Integer.MAX_VALUE;
+    private int val = Integer.MAX_VALUE;
     public int closestValue(TreeNode root, double target) {
-        val = root.val;
-        diff = Math.abs(root.val - target);
-
         dfs(root, target);
+
         return val;
     }
 
     public void dfs(TreeNode root, double target){
+        // return condition
         if (root == null) return;
 
-        double d = Math.abs(root.val - target);
-        if (d < diff){
-            diff = d;
+        if (Math.abs(root.val - target) < diff || (Math.abs(root.val - target) == diff && root.val < val)){
+            diff = Math.abs(root.val - target);
             val = root.val;
         }
-
         dfs(root.left, target);
         dfs(root.right, target);
     }
