4c8a6955fd8e3df096b5f7aa02217ead8f88f40d
Logan Wong <huang1002788835@outlook.com>
2026-03-16 21:42:55 -0700

    2402

--- src/main/java/com/dequeue_and_priorityqueue/MeetingRoomsIII_2402.java
+++ src/main/java/com/dequeue_and_priorityqueue/MeetingRoomsIII_2402.java
@@ -7,53 +7,49 @@ import java.util.PriorityQueue;
 public class MeetingRoomsIII_2402 {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(i -> i[0]));
-        PriorityQueue<int[]> inUsedQue = new PriorityQueue<>((i, j) -> {
-            if (i[0] == j[0]) return i[1] - j[1];
-            else return i[0] - j[0];
-        }); // int[0] -> endTime, int[1] -> room Number
-        PriorityQueue<Integer> unusedQue = new PriorityQueue<>();
+        PriorityQueue<long[]> inUsed = new PriorityQueue<>((i, j) -> {
+            if (i[1] == j[1]) return Long.compare(i[0], j[0]);
+            return Long.compare(i[1], j[1]);
+        }); // {idx, ending time}
+        PriorityQueue<Integer> unUsed = new PriorityQueue<>();
+        int[] fre = new int[n];
+        for (int i = 0; i < n; i++) unUsed.add(i);
+
+        for (int[] m: meetings){
+            long start = m[0], end = m[1];
+
+            while (!inUsed.isEmpty() && inUsed.peek()[1] <= start) {
+                unUsed.add((int) inUsed.poll()[0]);
+            }
+
+            if (!unUsed.isEmpty()) {
+                int availableRoom = unUsed.poll();
+                fre[availableRoom]++;
+                inUsed.add(new long[]{availableRoom, end});
+            } else {
+                long[] room = inUsed.poll();
+                fre[(int) room[0]]++;
+                long during = end - start;
+                inUsed.add(new long[]{room[0], room[1] + during});
+            }
+        }
+
+        int room = -1, f = 0;
         for (int i = 0; i < n; i++) {
-            unusedQue.add(i);
-        }
-        int[] timesUsed = new int[n];
-
-        for (int[] meeting: meetings){
-            int startTime = meeting[0], endTime = meeting[1], duration = endTime - startTime;
-
-            while (!inUsedQue.isEmpty() && inUsedQue.peek()[0] <= startTime){
-                int[] p = inUsedQue.poll();
-                unusedQue.add(p[1]);
-            }
-
-            if (!unusedQue.isEmpty()){
-                int meetingRoom = unusedQue.poll();
-                timesUsed[meetingRoom]++;
-
-                inUsedQue.add(new int[]{endTime, meetingRoom});
-            }else {
-                int[] tem = inUsedQue.poll();
-                timesUsed[tem[1]]++;
-
-                int newStartTime = Math.max(tem[0], startTime);
-                inUsedQue.add(new int[]{newStartTime + duration, tem[1]});
-            }
-
-        }
-
-        int maxx = 0, res = 0;
-        for (int i = 0; i < n; i++) {
-            if (timesUsed[i] > maxx){
-                maxx = timesUsed[i];
-                res = i;
+            if (fre[i] > f){
+                room = i;
+                f = fre[i];
             }
         }
 
-        return res;
+        return room;
     }
 
     public static void main(String[] args) {
         MeetingRoomsIII_2402 m = new MeetingRoomsIII_2402();
-        m.mostBooked(4, new int[][]{{18,19}, {3, 12}, {17, 19}, {2, 13}, {7, 10}});
-        // m.mostBooked(3, new int[][]{{1,20},{2, 10}, {3, 5}, {4, 9}, {6, 8}});
+        // m.mostBooked(4, new int[][]{{18,19}, {3, 12}, {17, 19}, {2, 13}, {7, 10}});
+        m.mostBooked(3, new int[][]{{1,20},{2, 10}, {3, 5}, {4, 9}, {6, 8}});
+
+        // inUsed: {2, 10} , {1, 10}, {0, 20},
     }
 }
