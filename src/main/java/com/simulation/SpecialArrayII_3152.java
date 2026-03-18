eb7d6768061b05847f1235420a5e889f809b0be1
Logan Wong <huang1002788835@outlook.com>
2026-03-17 18:26:46 -0700

    3152

--- src/main/java/com/simulation/SpecialArrayII_3152.java
+++ src/main/java/com/simulation/SpecialArrayII_3152.java
@@ -8,26 +8,19 @@ import java.util.List;
 public class SpecialArrayII_3152 {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         int n = nums.length;
-        List<int[]> specialArr = new ArrayList<>();
-        int start = 0;
+        int[] prefix = new int[n]; // prefix[i] -> 0 -> i, how many break points in this range
+        // [j, i] -> prefix[i] - prefix[j]
+        // == 0, it is a special array
         for (int i = 1; i < n; i++) {
-            if (nums[i] % 2 == nums[i - 1] % 2){
-                specialArr.add(new int[]{start, i - 1});
-                start = i;
-            }
+            if (nums[i] % 2 == nums[i - 1] % 2) prefix[i] = prefix[i - 1] + 1;
+            else prefix[i] = prefix[i - 1];
         }
-        specialArr.add(new int[]{start, n - 1});
 
-        specialArr.sort(Comparator.comparingInt(i -> i[0]));
         boolean[] res = new boolean[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int[] q = queries[i];
-            for (int[] s: specialArr){
-                if (s[0] <= q[0] && s[1] >= q[1]) {
-                    res[i] = true;
-                    break;
-                }
-            }
+        int i = 0;
+        for(int[] q: queries){
+            int start = q[0], end = q[1];
+            res[i++] = prefix[start] - prefix[end] == 0;
         }
 
         return res;
