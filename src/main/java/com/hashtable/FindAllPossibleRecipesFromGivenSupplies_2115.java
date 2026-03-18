f51889652ad006e78227f519bb51afe5ef92fed9
Logan Wong <huang1002788835@outlook.com>
2026-03-17 18:00:54 -0700

    2115

--- src/main/java/com/hashtable/FindAllPossibleRecipesFromGivenSupplies_2115.java
+++ src/main/java/com/hashtable/FindAllPossibleRecipesFromGivenSupplies_2115.java
@@ -4,40 +4,38 @@ import java.util.*;
 
 public class FindAllPossibleRecipesFromGivenSupplies_2115 {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Map<String, Set<String>> recToIng = new HashMap<>();
-        for (int i = 0; i < recipes.length; i++) recToIng.put(recipes[i], new HashSet<>(ingredients.get(i)));
+        HashMap<String, List<String>> recToIng = new HashMap<>();
+        for (int i = 0; i < recipes.length; i++) recToIng.put(recipes[i], ingredients.get(i));
         HashSet<String> suppliesSet = new HashSet<>(Arrays.asList(supplies));
 
-        List<String> result = new ArrayList<>();
-        HashMap<String, Integer> status = new HashMap<>(); // 1 -> can't make; 2 -> searching; 3-> can make it
-
-        for (String rep: recipes){
-            if (dfs(recToIng, suppliesSet, rep, status)) result.add(rep);
+        Map<String, Integer> status = new HashMap<>(); // 1 -> searching, 2 -> can't make , 3 -> can make
+        List<String> res = new ArrayList<>();
+        for (String rec:recipes){
+            if (dfs(recToIng, suppliesSet, rec, status)) res.add(rec);
         }
 
-        return result;
+        return res;
     }
 
-    public boolean dfs(Map<String, Set<String>> recToIng, HashSet<String> suppliesSet,
-                       String recipe, HashMap<String, Integer> status){
+    public boolean dfs(HashMap<String, List<String>> recToIng, HashSet<String> suppliesSet, String rec, Map<String, Integer> status){
         // return condition
-        if (suppliesSet.contains(recipe)) return true;
-        if (!recToIng.containsKey(recipe)) return false;
-        int s = status.getOrDefault(recipe, 0);
+        if (suppliesSet.contains(rec)) return true;
+        if (!recToIng.containsKey(rec)) return false;
+        int s = status.getOrDefault(rec, 0);
         if (s == 1 || s == 2) return false;
         if (s == 3) return true;
 
-        status.put(recipe, 2);
-
-        for (String ing: recToIng.get(recipe)){
-            if (!dfs(recToIng, suppliesSet, ing, status)){
-                status.put(ing, 1);
+        status.put(rec, 1);
+        List<String> ings = recToIng.get(rec);
+        for (String i: ings){
+            if (!dfs(recToIng, suppliesSet, i, status)){
+                status.put(i, 2);
                 return false;
             }
         }
 
-        status.put(recipe, 3);
-        suppliesSet.add(recipe);
+        status.put(rec, 3);
+        suppliesSet.add(rec);
         return true;
     }
 
