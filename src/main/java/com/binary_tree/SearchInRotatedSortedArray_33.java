a37893cc2b7612dda074a9eaf712a2d4578c0637
Logan Wong <huang1002788835@outlook.com>
2026-03-24 23:35:56 -0700

    33

--- src/main/java/com/binary_tree/SearchInRotatedSortedArray_33.java
+++ src/main/java/com/binary_tree/SearchInRotatedSortedArray_33.java
@@ -3,32 +3,35 @@ package com.binary_tree;
 public class SearchInRotatedSortedArray_33 {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int pivot = 0, right = n, model = nums[0];
-        while (pivot < right){
-            int mid = pivot + (right - pivot) / 2;
-            if (nums[mid] >= model) {
-                pivot = mid + 1;
-            }else {
+        int turnPoint = 0;
+        while (turnPoint < n){
+            if (turnPoint - 1 >= 0 && nums[turnPoint - 1] > nums[turnPoint]) break;
+            turnPoint++;
+        }
+
+        int left = 0, right = turnPoint;
+        // do binary search from 0 - turn point;
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target){
                 right = mid;
+            }else {
+                left = mid + 1;
             }
         }
 
-        int l, h;
-        if (pivot == n){
-            l = 0; h = n - 1;
-        } else if (target >= model) {
-            l = 0; h = pivot - 1;
-        }else {
-            l = pivot; h = n - 1;
-        }
+        if (turnPoint == n) return -1;
 
-        while (l <= h){
-            int mid = l + (h - l) / 2;
+        left  = turnPoint;
+        right = n;
+        while (left < right){
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
-            if (nums[mid] < target){
-                l = mid + 1;
+            if (nums[mid] > target){
+                right = mid;
             }else {
-                h = mid - 1;
+                left = mid + 1;
             }
         }
 
@@ -38,6 +41,7 @@ public class SearchInRotatedSortedArray_33 {
     public static void main(String[] args) {
         SearchInRotatedSortedArray_33 s= new SearchInRotatedSortedArray_33();
         // s.search(new int[]{4,5,6,7,0,1,2}, 0);
-         s.search(new int[]{1, 3}, 0);
+        // s.search(new int[]{3, 1}, 3);
+        s.search(new int[]{1}, 0);
     }
 }
