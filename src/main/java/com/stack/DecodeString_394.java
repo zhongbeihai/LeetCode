a9ae573d9e899b587c631481a29e0abe17ac967d
Logan Wong <huang1002788835@outlook.com>
2026-03-24 21:44:38 -0700

    394

--- src/main/java/com/stack/DecodeString_394.java
+++ src/main/java/com/stack/DecodeString_394.java
@@ -8,36 +8,34 @@ import java.util.Stack;
 
 public class DecodeString_394 {
     public String decodeString(String s) {
-        Deque<Integer> numStack = new ArrayDeque<>();
-        Deque<StringBuffer> stringStack = new ArrayDeque<>();
+        ArrayDeque<Integer> numStack = new ArrayDeque<>();
+        ArrayDeque<StringBuilder> stringStack = new ArrayDeque<>();
 
+        StringBuilder cur = new StringBuilder();
         int num = 0;
-        StringBuffer curBuilder = new StringBuffer();
-        for(char c: s.toCharArray()){
+        for (char c: s.toCharArray()){
             if (Character.isDigit(c)){
                 num = num * 10 + c - '0';
+            } else if (Character.isLetter(c)) {
+                cur.append(c);
             } else if (c == '[') {
                 numStack.push(num);
-                stringStack.push(curBuilder);
-
-                curBuilder = new StringBuffer();
                 num = 0;
-            }else if(Character.isLetter(c)){
-                curBuilder.append(c);
+                stringStack.push(cur);
+                cur = new StringBuilder();
             }else if (c == ']'){
-                int rep = numStack.poll();
-                String base = curBuilder.toString();
-                for (int i = 1; i < rep; i++) {
-                    curBuilder.append(base);
+                int r = numStack.pop();
+                String basic = cur.toString();
+                for (int i = 1; i < r; i++) {
+                    cur.append(basic);
                 }
-
-                StringBuffer prev = stringStack.pop();
-                prev.append(curBuilder);
-                curBuilder = prev;
+                StringBuilder lastBuilder = stringStack.pop();
+                lastBuilder.append(cur);
+                cur = lastBuilder;
             }
         }
 
-        return curBuilder.toString();
+        return cur.toString();
     }
 
     public static void main(String[] args) {
