861d289cc0a56dc2e62dfe211418eca00fbc23f5
Logan Wong <huang1002788835@outlook.com>
2026-03-26 18:36:20 -0700

    15

--- src/main/java/com/two_pointers/ThreeSum_15.java
+++ src/main/java/com/two_pointers/ThreeSum_15.java
@@ -6,21 +6,20 @@ import java.util.List;
 
 public class ThreeSum_15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) return new ArrayList<>();
-
         Arrays.sort(nums);
+
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++){
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) break;
-            if (i > 1 && nums[i] == nums[i - 1]) continue;
+            if (i > 1 && nums[i] == nums[i - 1]) continue;;
 
             int left = i + 1, right = nums.length - 1;
             while (left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0) {
+                if (sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    while (left < right && nums[left + 1] == nums[left]) left++;
+                    while (left < right && nums[right - 1] == nums[right]) right--;
                     left++;
                     right--;
                 }
