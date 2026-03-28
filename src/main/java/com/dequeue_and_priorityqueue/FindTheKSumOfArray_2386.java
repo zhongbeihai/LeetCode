0cc025c14489b49de581c3997ae6def5dce4781f
Logan Wong <huang1002788835@outlook.com>
2026-03-28 14:54:41 -0700

    2386

--- src/main/java/com/dequeue_and_priorityqueue/FindTheKSumOfArray_2386.java
+++ src/main/java/com/dequeue_and_priorityqueue/FindTheKSumOfArray_2386.java
@@ -6,33 +6,45 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class FindTheKSumOfArray_2386 {
+    // we take all the positive number -> maximumSum
+    // we transfer the nums[i] -> loss -> each number is a loss to the maximum sum
+    // search the loss
+    //      not take a positive/ take negative number
+    // we can use a priorityQueue to search
+    // we can use an int[]{loss, idx}
     public long kSum(int[] nums, int k) {
+        int n = nums.length;
         long maximumSum = 0L;
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < n; i++){
             if (nums[i] > 0) maximumSum += nums[i];
-            else nums[i] = - nums[i];
+            else {
+                nums[i] = -nums[i]; // transfer the nums into losses
+            }
         }
+        if (k == 1) return maximumSum;
 
         Arrays.sort(nums);
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(i -> i[0])); // {accLoss, index};
-        pq.add(new long[]{0L, 0L});
 
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(i -> i[0])); // {curLoss, idx}
+        pq.add(new long[]{nums[0], 0});
+
+        // we want find k -1 smallest loss
+        long loss = 0L;
         for (int i = 1; i < k; i++) {
             long[] cur = pq.poll();
-            long curLoss = cur[0], curIndex = cur[1];
+            loss = cur[0];
+            int curIdx = Math.toIntExact(cur[1]);
 
-            if (curIndex + 1 < nums.length){
-                // 1. add next loss
-                long nextLoss = nums[Math.toIntExact(curIndex + 1)];
-                pq.add(new long[]{curLoss + nextLoss, curIndex + 1});
+            if(curIdx + 1 < n){
+                // keep adding nums[i + 1] as loss
+                pq.add(new long[]{loss + nums[curIdx + 1], curIdx + 1});
 
-                // 2. remove last loss and add net loss
-                long lastLoss = nums[Math.toIntExact(curIndex)];
-                pq.add(new long[]{curLoss - lastLoss + nextLoss, curIndex + 1});
+                // remove loss nums[i], then we add nums[i + 1]
+                pq.add(new long[]{loss - nums[curIdx] + nums[curIdx + 1], curIdx + 1});
             }
         }
 
-        return maximumSum - pq.poll()[0];
+        return maximumSum - loss;
     }
 
     public static void main(String[] args) {
