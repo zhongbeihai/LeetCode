3a7baf2c540c1f0d3895985e7e961363f6e03f7c
Logan Wong <huang1002788835@outlook.com>
2026-03-29 20:23:11 -0700

    2402

--- src/main/java/com/dequeue_and_priorityqueue/MeetingRoomsIII_2402.java
+++ src/main/java/com/dequeue_and_priorityqueue/MeetingRoomsIII_2402.java
@@ -5,44 +5,52 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MeetingRoomsIII_2402 {
+    // sort the meetings by its start time
+    // use two priorityQueue ->
+    // 1. maintain the meeting rooms in used  (in the order of end time)
+    // 2. the meeting room that are available(in the order of the number)
+    // traverse all the meetings
+    // record the meeting room that used the most
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(i -> i[0]));
-        PriorityQueue<long[]> inUsed = new PriorityQueue<>((i, j) -> {
+        PriorityQueue<Integer> availableRoom = new PriorityQueue<>(); // in the order the room number
+        PriorityQueue<long[]> roomInUsed = new PriorityQueue<>((i, j) -> {
             if (i[1] == j[1]) return Long.compare(i[0], j[0]);
             return Long.compare(i[1], j[1]);
-        }); // {idx, ending time}
-        PriorityQueue<Integer> unUsed = new PriorityQueue<>();
-        int[] fre = new int[n];
-        for (int i = 0; i < n; i++) unUsed.add(i);
+        }); // {room number, end time of the meetings}
+        for (int i = 0; i < n; i++) availableRoom.add(i);
 
+        int[] count = new int[n];
+        // traversal all the meetings
         for (int[] m: meetings){
-            long start = m[0], end = m[1];
-
-            while (!inUsed.isEmpty() && inUsed.peek()[1] <= start) {
-                unUsed.add((int) inUsed.poll()[0]);
+            int start = m[0], end = m[1], duration = end - start;
+            // release all the rooms that become available
+            while (!roomInUsed.isEmpty() && start >= roomInUsed.peek()[1]){
+                availableRoom.add((int) roomInUsed.poll()[0]);
             }
 
-            if (!unUsed.isEmpty()) {
-                int availableRoom = unUsed.poll();
-                fre[availableRoom]++;
-                inUsed.add(new long[]{availableRoom, end});
-            } else {
-                long[] room = inUsed.poll();
-                fre[(int) room[0]]++;
-                long during = end - start;
-                inUsed.add(new long[]{room[0], room[1] + during});
-            }
-        }
-
-        int room = -1, f = 0;
-        for (int i = 0; i < n; i++) {
-            if (fre[i] > f){
-                room = i;
-                f = fre[i];
+            // there all still available meeting rooms
+            if (!availableRoom.isEmpty()){
+                int roomNumber = availableRoom.poll();
+                count[roomNumber]++;
+                roomInUsed.add(new long[]{roomNumber, end});
+            }else { // not any meeting room is available
+                // we must wait for the room that will be released earliest
+                long[] room = roomInUsed.poll();
+                int roomNumber = (int) room[0];
+                count[roomNumber]++;
+                roomInUsed.add(new long[]{roomNumber, room[1] + duration});
             }
         }
 
-        return room;
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (count[i] > count[ans]) {
+                ans = i;
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
